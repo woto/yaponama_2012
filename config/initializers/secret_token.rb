9e41bc970eb7def0863e47e9d50d507934b4e8ae
Руслан Корнев <oganer@gmail.com>
2012-06-26 13:57:50 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaponama2012::Application.config.secret_token = 'b99d7d7a9b4d84f307b0720f65f7c14e7b990a16c9d6280fec3677c0c650760461497da88d0163479821e7a8fd7ffbd3a6aa65d3453ffe36a067d4406f54b418'
