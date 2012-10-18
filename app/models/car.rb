647a4b61a8d81cff43f0de2154dc6ccd0ddce7c6
Руслан Корнев <oganer@gmail.com>
2012-10-18 21:45:44 +0400

    Очень неприятную помарку сделал, не мог разобрать почему не могу удалять запросы. Забыл в car ... allow_destroy для requests

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,7 +5,7 @@ class Car < ActiveRecord::Base
   belongs_to :user, :inverse_of => :cars
   attr_accessible :dvigatel, :frame, :god, :kod_dvigatelya, :kod_kuzova, :komplektaciya, :kpp, :marka, :model, :moschnost, :privod, :rinok, :tip, :tip_kuzova, :vin, :invisible, :user_id, :requests_attributes
   validates :user, :presence => true
-  accepts_nested_attributes_for :requests
+  accepts_nested_attributes_for :requests, :allow_destroy => true
 
   def to_label
     "#{dvigatel} - #{frame} - #{god} - #{kod_dvigatelya} - #{kod_kuzova} - #{komplektaciya} - #{kpp} - #{marka} - #{model} - #{moschnost} - #{privod} - #{rinok} - #{tip} - #{tip_kuzova} - #{vin} - #{invisible}"
