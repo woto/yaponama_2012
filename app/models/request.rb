2d098a422e3e5bea835b92add46c20bc2d5d3602
Руслан Корнев <oganer@gmail.com>
2012-10-26 19:06:22 +0400

    Тестировал интерфейс свертки/развертки на Request.

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,7 +5,7 @@ class Request < ActiveRecord::Base
   belongs_to :car, :inverse_of => :requests
   has_many :requests, :inverse_of => :request, :dependent => :destroy
   belongs_to :request, :inverse_of => :requests
-  attr_accessible :catalog_number, :invisible, :manufacturer, :notes, :creation_reason, :check_needed, :user_id, :car_id, :request_id, :requests_attributes, :name
+  attr_accessible :catalog_number, :invisible, :manufacturer, :notes, :creation_reason, :check_needed, :user_id, :car_id, :request_id, :requests_attributes, :name, :visible
   accepts_nested_attributes_for :requests, :allow_destroy => true
 
   def to_label
