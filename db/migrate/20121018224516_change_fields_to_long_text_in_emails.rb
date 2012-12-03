1b936d0fa678bb206c477eb9984a463cbd0bdb41
Руслан Корнев <oganer@gmail.com>
2012-12-03 18:10:02 +0400

    Попытка переезда на PostgreSQL

--- db/migrate/20121018224516_change_fields_to_long_text_in_emails.rb
+++ db/migrate/20121018224516_change_fields_to_long_text_in_emails.rb
@@ -1,13 +1,15 @@
-class ChangeFieldsToLongTextInEmails < ActiveRecord::Migration
-  def up
-    change_column :emails, :body, :text, :limit => 4294967295
-    change_column :emails, :html_part, :text, :limit => 4294967295
-    change_column :emails, :text_part, :text, :limit => 4294967295
-  end
-
-  def down
-    change_column :emails, :body, :text
-    change_column :emails, :html_part
-    change_column :emails, :text_part
-  end
-end
+# Как я понимаю постгрес не может менять размер текстового поля
+#
+#class ChangeFieldsToLongTextInEmails < ActiveRecord::Migration
+#  def up
+#    change_column :emails, :body, :text, :limit => 4294967295
+#    change_column :emails, :html_part, :text, :limit => 4294967295
+#    change_column :emails, :text_part, :text, :limit => 4294967295
+#  end
+#
+#  def down
+#    change_column :emails, :body, :text
+#    change_column :emails, :html_part
+#    change_column :emails, :text_part
+#  end
+#end
