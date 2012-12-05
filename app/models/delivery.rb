65a88e142e643c2df57dbf5dc91da75a65cd34ac
Руслан Корнев <oganer@gmail.com>
2012-12-05 23:47:14 +0400

    Из-за того что отсутствовала валидация из временного seed заполнялся пустыми значениями, поэтому неправильно считалась цена.

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,4 +4,8 @@ class Delivery < ActiveRecord::Base
   has_many :orders
   belongs_to :delivery_category
   mount_uploader :image, DeliveryImageUploader
+
+  validates :full_prepayment_required, :phone_required, :name_required, :postal_address_required, :delivery_cost_required, :inclusion => { :in => [true, false] }
+  validates :available, :inclusion => { :in => [true, false] }
+  validates :delivery_category_id, :presence => true
 end
