8e587eceaf2a5242865d1895cf5c16d6e2401ed9
Руслан Корнев <oganer@gmail.com>
2012-12-11 23:47:07 +0400

    Удаление давно не используемого carts_controller

--- app/controllers/admin/carts_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::ProductsController < Admin::ApplicationController
-  def index
-    @products = Product.where(:status => :incart)
-
-    respond_to do |format|
-      format.html
-    end
-  end
-end
