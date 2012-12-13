64e3bb4d71bbe5a675d37b926503177fb3a7975b
Руслан Корнев <oganer@gmail.com>
2012-12-13 23:48:17 +0400

    Подключил Database Cleaner и сразу избавился от ошибки когда браузер открывался и писал Internal Server Error.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,15 @@ require 'rspec/autorun'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean_with(:truncation)
+
+
 RSpec.configure do |config|
+
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -18,12 +26,12 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
