6f469aa3c76f6687cb33bc2e5968202daefa6d5c
Руслан Корнев <oganer@gmail.com>
2012-12-16 18:03:39 +0400

    Актуализировал factory -> phone и email_address - added_by. К позапрошлому коммиту.

--- spec/factories/email_addresses.rb
+++ spec/factories/email_addresses.rb
@@ -1,12 +1,17 @@
 # encoding: utf-8
 
 FactoryGirl.define do
+
   sequence :random_email_address do |n|
     "email_address_#{n}@example.ru"
   end
 
   factory :email_address do
     email_address{ generate(:random_email_address) }
+
+    after(:build) do |ea, evaluator|
+      ea.added_by = generate(:random_added_by)
+    end
 
     factory :email_address_with_user do
       association :user, factory: :minimal_valid_user
--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -6,6 +6,10 @@ FactoryGirl.define do
     n + 1000000000
   end
 
+  sequence :random_added_by do |n|
+    ['Менеджером', 'Покупателем'].sample
+  end
+
   sequence :random_can_receive_sms do |n|
     ['Да', 'Нет', 'Неизвестно'].sample
   end
@@ -13,7 +17,12 @@ FactoryGirl.define do
   factory :phone do
 
     phone{ generate(:random_phone) }
+
     can_receive_sms{ generate(:random_can_receive_sms)}
+
+    after(:build) do |p, evaluator|
+      p.added_by = generate(:random_added_by)
+    end
 
     factory :phone_with_user do
       association :user, factory: :minimal_valid_user
