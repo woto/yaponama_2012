3aef06ebf88d4de35f2df271fc947768d826ac58
Руслан Корнев <oganer@gmail.com>
2012-12-17 21:35:02 +0400

    В Edit контроллер так же добавлено состояние cancel для возможности 'Перезаказа'

--- app/controllers/admin/products/edit_controller.rb
+++ app/controllers/admin/products/edit_controller.rb
@@ -9,7 +9,7 @@ class Admin::Products::EditController < Admin::ProductsController
       @products = products_user_order_tab_scope( Product.order("updated_at DESC"), 'checked' )
       products_any_checked_validation
       products_only_one_validation
-      products_all_statuses_validation ['incart', 'inorder', 'ordered', 'pre_supplier', 'post_supplier', 'stock', 'complete']
+      products_all_statuses_validation ['incart', 'inorder', 'ordered', 'pre_supplier', 'post_supplier', 'stock', 'complete', 'cancel']
 
     rescue ValidationError => e
       redirect_to :back, :alert => e.message
