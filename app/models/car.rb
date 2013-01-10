b54b9407436f67386130ff833f68bdc930323333
Руслан Корнев <oganer@gmail.com>
2013-01-10 04:46:50 +0400

    Мелочь. Синтаксическая ошибка в model -> car

--- app/models/car.rb
+++ app/models/car.rb
@@ -17,7 +17,7 @@ class Car < ActiveRecord::Base
 
   attr_accessible :car_number, :notes, :dvigatel, :frame, :god, :kod_dvigatelya, :kod_kuzova, :komplektaciya, :kpp, :marka, :model, :moschnost, :privod, :rinok, :tip, :tip_kuzova, :vin, :notes_invisible, :visible, :as => [:admin, :manager, :user]
 
-  validates :vin, :uniqueness => { case_sensitive: false }, :allow_blank => true
+  validates :vin, :uniqueness => { case_sensitive: false }, :allow_blank => true, :length => { :within => 17..17 }
   validates :frame, :uniqueness => { case_sensitive: false }, :allow_blank => true
 
   def to_label
