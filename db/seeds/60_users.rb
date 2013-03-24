95ebdf6d2d0b4f56b34c5f68bb3de6b1ec12b73f
Ruslan Kornev <oganer@gmail.com>
2013-03-24 23:39:17 +0400

    Мелочь. Имя начального юзера. Т.к. все таки предполагается что на сайте не будет людей без имён.

--- db/seeds/60_users.rb
+++ db/seeds/60_users.rb
@@ -5,6 +5,7 @@ user.build_account
 user.account.assign_attributes( {:debit => 0, :credit => 0} )
 user.assign_attributes( SiteConfig.default_user_attributes )
 user.role = "admin"
+user.names.new(:name => "Администратор", :creation_reason => "manager")
 user.password = '1111111111'
 phone = user.phones.build
 phone.assign_attributes({:phone => '1111111111', :phone_type => 'mobile_russia', :creation_reason => 'manager'} )
