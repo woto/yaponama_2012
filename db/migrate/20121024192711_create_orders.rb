9a21d0bfd69cbde5435ee72b1d95482409aaa60a
Ruslan Kornev <oganer@gmail.com>
2013-04-20 03:43:22 +0400

    Мелочь. Исправил ошибочку в миграции (получилось name_id_id).

--- db/migrate/20121024192711_create_orders.rb
+++ db/migrate/20121024192711_create_orders.rb
@@ -1,10 +1,10 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.references :name_id, index: true
-      t.references :postal_address_id, index: true
-      t.references :metro_id, index: true
-      t.references :shop_id, index: true
+      t.references :name, index: true
+      t.references :postal_address, index: true
+      t.references :metro, index: true
+      t.references :shop, index: true
       t.decimal :delivery_cost, :precision => 8, :scale => 2
       t.string  :status
       t.references :delivery, index: true
