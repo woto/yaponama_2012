2763dd52dbf42bdf3e6cc410ea1b628df6b1c410
Ruslan Kornev <oganer@gmail.com>
2013-05-08 09:13:33 +0400

    Еще один

--- db/migrate/20121207143942_create_admin_metro.rb
+++ db/migrate/20121207143942_create_admin_metro.rb
@@ -1,10 +1,6 @@
 class CreateAdminMetro < ActiveRecord::Migration
   def change
     create_table :metro do |t|
-      t.string :metro
-      t.integer :delivery_cost
-      t.references :delivery, index: true
-
       t.timestamps
     end
   end
--- db/migrate/20130416140308_create_shops.rb
+++ db/migrate/20130416140308_create_shops.rb
@@ -1,10 +1,6 @@
 class CreateShops < ActiveRecord::Migration
   def change
     create_table :shops do |t|
-      t.string :name
-      t.text :content
-      t.references :delivery
-
       t.timestamps
     end
   end
