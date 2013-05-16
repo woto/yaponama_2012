dc137e3a544ef2284c7c781da83128d795735446
Ruslan Kornev <oganer@gmail.com>
2013-05-16 23:47:54 +0400

    Переименовал миграцию в 9999... в надежде, что это правильный ход. Кстати, этот коммит будет squashed. И добавил в transactionables passport.

--- db/migrate/99999999999999_transactionables.rb
+++ db/migrate/99999999999999_transactionables.rb
@@ -1,6 +1,6 @@
 class Transactionables < ActiveRecord::Migration
   def self.up
-    [:phones, :names, :email_addresses, :postal_addresses, :cars, :products, :companies, :accounts, :orders].each do |table_name|
+    [:phones, :names, :email_addresses, :passports, :postal_addresses, :cars, :products, :companies, :accounts, :orders, :profiles].each do |table_name|
       create_table "#{table_name.to_s.singularize}_transactions".to_sym do |t|
 
         table_model = table_name.to_s.singularize.camelize.constantize
@@ -25,12 +25,18 @@ class Transactionables < ActiveRecord::Migration
         table_model.columns.each do |column|
           # ID Отслеживаемого объекта никогда не может меняться,
           # поэтому следить за ним не надо. В отличии от supplier_id (в противовес user_id)
+          # TODO
+          #if table_model == Account && ["accountable_id", "accountable_type"].include?(column.name)
+          #  eval "t.#{column.type.to_s} :#{column.name}"
+          #elsif !(["user_id", "id", "created_at", "updated_at"].include?(column.name))
           if column.name != "id"
             eval "t.#{column.type.to_s} :#{column.name}_before"
             eval "t.#{column.type.to_s} :#{column.name}_after"
           end
         end
 
+        # TODO
+        #t.column :created_at, :datetime
         t.timestamps
 
       end
