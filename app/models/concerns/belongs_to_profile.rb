ce6095f3f7f7722debfb041bfb33d3ae5bc2e52e
Ruslan Kornev <oganer@gmail.com>
2013-05-17 00:30:19 +0400

    Добавлен необходимый belongs_to_profile

---
+++ app/models/concerns/belongs_to_profile.rb
@@ -0,0 +1,15 @@
+module BelongsToProfile
+  extend ActiveSupport::Concern
+
+  included do
+    validates :profile, :presence => true
+    
+    before_save :copy_user_id_from_profile_to_self
+
+    def copy_user_id_from_profile_to_self
+      self.user_id = profile.user_id
+    end
+
+  end
+
+end
