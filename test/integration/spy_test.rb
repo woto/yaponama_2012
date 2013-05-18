4c6a8b487fb2c92c0cd8c030a2133c436b3095d6
Ruslan Kornev <oganer@gmail.com>
2013-05-18 19:44:19 +0400

    Забыл включить файл тестриования spy_test (уже давно болтается, там проверка ip, временной зоны, заголовков бразера)

--- test/integration/spy_test.rb
+++ test/integration/spy_test.rb
@@ -10,7 +10,20 @@ class SpyTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'Если у клиента указано неправильное время, то не должно произойти ошибки и будет использоваться часовой пояс по-умолчанию' do
+  test 'Сначала мы сэмулируем заход пользователя с ip адресом 85.117.95.1 и обнаружим, что пользователь с Норильска, запишем ip, город и регион, а потом зайдем с 127.0.0.1 запишем новый ip, а город и регион обнулятся' do
+    get '/', {}, { 'REMOTE_ADDR' => '85.117.95.1' }
+    assert_equal User.last.remote_ip, '85.117.95.1'
+    assert_equal User.last.ipgeobase_name, 'Норильск'
+    assert_equal User.last.ipgeobase_names_depth_cache, 'Россия/Сибирский федеральный округ/Красноярский край'
+
+    get '/', {}, { 'REMOTE_ADDR' => '127.0.0.1' }
+    assert_equal User.last.remote_ip, '127.0.0.1'
+    assert_equal User.last.ipgeobase_name, ''
+    assert_equal User.last.ipgeobase_names_depth_cache, ''
+  end
+
+  test 'Если на стороне клиента установлено неверное время, то ошибки произойти не должно, а будет установлен часовой пояс по-умолчанию. Если клиент изменит время на своем компьютере на верное, напр. +3 часа (плюс минус погрешности в минутах), то соответсвенно у него должен выставиться часовой пояс +3' do
+
     post '/stats', {
       russian_time_zone_auto: Time.now - 1.year,
       stat: {
@@ -20,9 +33,28 @@ class SpyTest < ActionDispatch::IntegrationTest
       }
     }
 
-    get '/'
-    assert_equal '200', @response.code
+    assert_equal User.last.russian_time_zone_auto_id, 4
 
+    post '/stats', {
+      russian_time_zone_auto: Time.now + 3.hours + 10.minutes,
+      stat: {
+        location: '' ,
+        title:  '',
+        referrer: '',
+      }
+    }
+
+    assert_equal User.last.russian_time_zone_auto_id, 3
+  end
+
+  test 'Мы должны запоминать переданные клиентом user_agent и accept_language как есть. Аналогично как и с ip, если эти аттрибуты меняются, то мы так же сохраняем новые значения' do
+    get '/', {}, { 'User-Agent' => 'Browser 1', 'Accept-Language' => 'Language 1' }
+    assert_equal User.last.user_agent, 'Browser 1'
+    assert_equal User.last.accept_language, 'Language 1'
+
+    get '/', {}, { 'User-Agent' => 'Browser 2', 'Accept-Language' => 'Language 2' }
+    assert_equal User.last.user_agent, 'Browser 2'
+    assert_equal User.last.accept_language, 'Language 2'
   end
 
 end
