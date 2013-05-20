ad84ab10e6b00c77322b9cce58b6757caa04063a
Ruslan Kornev <oganer@gmail.com>
2013-05-21 01:44:07 +0400

    Хочу попробовать сделать тестирование oauth полным (с загрузкой google и ввода данных там.)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,9 @@
 #OmniAuth.config.logger = Rails.logger
 
-#SERVICES = YAML.load(File.open("#{::Rails.root}/config/oauth.yml").read)
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
 
-  provider :google_oauth2, SiteConfig.google_oauth2_key, SiteConfig.google_oauth2_secret, {:access_type => 'online', :approval_prompt => ''} # if SERVICES['google']
+  provider :google_oauth2, SiteConfig.google_oauth2_key, SiteConfig.google_oauth2_secret, {:access_type => 'online', :approval_prompt => ''}
   provider :facebook, SiteConfig.facebook_key, SiteConfig.facebook_secret
   provider :vkontakte, SiteConfig.vkontakte_key, SiteConfig.vkontakte_secret
   provider :twitter, SiteConfig.twitter_key, SiteConfig.twitter_secret
@@ -14,14 +12,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :mailru, SiteConfig.mailru_key, SiteConfig.mailru_secret
 end
 
-if Rails.env.test?
-  OmniAuth.config.test_mode = true
-
-  # Потом подставить тестируемые данные
-  #OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-  #  :provider => 'google_oauth2',
-  #  :uid => '123545'
-  #  # etc.
-  #})
-
-end
+#if Rails.env.test?
+#  OmniAuth.config.test_mode = true
+#
+#  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+#    :provider => 'google_oauth2',
+#    :uid => '123545'
+#  })
+#
+#end
