2e853f7899eb8526b54643f45d3f3a4b5462cf0a
Ruslan Kornev <oganer@gmail.com>
2013-05-22 00:15:34 +0400

    Дописал тесты на rspec для ситуаций, когда auth_token не существует в базе и для первого посещения.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,15 +3,16 @@
 require 'spec_helper'
 
 describe WelcomeController do
-  it "Если пользователь запрашивает страницу с несуществующим в базе пользователей auth_token'ом, то он должен быть перенаправлен на главную страницу. и auth_token должен быть сброшен." do
-    request.cookies['auth_token'] = 'auth_token_of_nonexistent_user'
+  it "Если посетитель запрашивает страницу с несуществующим auth_token'ом, то он должен быть перенаправлен на главную страницу и auth_token должен быть сброшен." do
+    cookies['auth_token'] = 'auth_token_of_nonexistent_user'
     get :index
-    response.should redirect_to('/')
-    expect(response.cookies['auth_token']).to eq(nil)
+    expect(response).to redirect_to('/')
+    expect(cookies['auth_token']).to be_nil
   end
 
-  it "Если сайт посещает новый пользователь, то должна создаться новая запись в базе пользователей" do
-    expect { get :index }.to change{ User.count }.by(1)
+  it "Если посетитель впервые посещает сайт, то в базе должен добавиться пользователь, а клиенту назначен auth_token соответствующий новой записи" do
+    expect {get :index}.to change{User.count}.by(1)
+    expect(response.cookies['auth_token']).not_to be_nil
+    expect(User.last.auth_token).to eq(response.cookies['auth_token'])
   end
-
 end
