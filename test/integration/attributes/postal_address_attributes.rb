fc1ff544f658f2a40896a5ee4fb5893bc83af2f2
Ruslan Kornev <oganer@gmail.com>
2013-05-23 02:57:04 +0400

    Добавил PostalAddressAttributes для использования в тестах

---
+++ test/integration/attributes/postal_address_attributes.rb
@@ -0,0 +1,15 @@
+module PostalAddressAttributes
+  def postal_address_attributes
+    {
+      'postal_address' => {
+        'postcode' => '123123',
+        'region' => '1',
+        'city' => '1',
+        'street' => '1',
+        'house' => '1',
+        'stand_alone_house' => '0',
+        'room' => '1'
+      }
+    }
+  end
+end
