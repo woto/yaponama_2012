cf73dfcdd95e5150380ca4d5cc9d501af4cbe9fa
Ruslan Kornev <oganer@gmail.com>
2013-05-28 08:07:07 +0400

    logout_from_all_places от лица пользователя

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class UsersController < ApplicationController
   before_action :only_authenticated, :only => [:show]
 
@@ -8,6 +10,12 @@ class UsersController < ApplicationController
       format.html # show.html.erb
       format.json { render :json => @user }
     end
+  end
+
+  def logout_from_all_places
+    @user.generate_token :auth_token
+    @user.save!
+    redirect_to root_path, notice: "Вы успешно вышли со всех компьютеров, где использовалась ваша учетная запись."
   end
 
 end
