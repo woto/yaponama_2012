7afa7da23e3e6c4c664fcf8246d6c2498da294d7
Ruslan Kornev <oganer@gmail.com>
2013-06-15 10:26:46 +0400

    Добавлен уже используемый sht_rails в application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,23 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
+//
 //= require jquery_ujs
 //= require jquery-ui
 //= require bootstrap/bootstrap
 //
+// == sht_rails
+//
+//= require handlebars
+//= require_tree ../../templates
 //
 //
-// console.log-wrapper
+// == console.log-wrapper
 //
 //= require console.log-wrapper/consolelog.min.js
 //= require console.log-wrapper/consolelog.detailprint.min.js
@@ -43,8 +36,6 @@
 //= require underscore/underscore
 
 //= require jquery-jsonp/src/jquery.jsonp
-
-//=require holder.js/holder
 
 //=require toolbar-checkboxes
 
@@ -80,3 +71,8 @@
 //= require 'deliveries'
 //= require 'auth'
 //= require 'catch_enter'
+// == holderjs
+//
+//= require holder.js/holder
+//= require holder
+
