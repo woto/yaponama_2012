c3bd45edebd7e0858effd0e1fe15ce4151fcf62a
Ruslan Kornev <oganer@gmail.com>
2013-09-05 14:03:21 +0400

    В миграцию Name добавлены фамилия и отчество

--- db/migrate/20130227004055_create_names.rb
+++ db/migrate/20130227004055_create_names.rb
@@ -1,7 +1,9 @@
 class CreateNames < ActiveRecord::Migration
   def change
     create_table :names do |t|
+      t.string :surname
       t.string :name
+      t.string :patronymic
       t.references :profile, index: true
 
       t.timestamps
