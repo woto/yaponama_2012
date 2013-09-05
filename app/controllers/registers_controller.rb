681f9f4d36acd1f28fafd35e6b102d39113db36c
Ruslan Kornev <oganer@gmail.com>
2013-09-05 13:44:19 +0400

    RegistersController

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -6,87 +6,34 @@ class RegistersController < ApplicationController
 
   def edit
     @user = current_user
-
-    if params[:with] == 'email'
-      if @user.email_addresses.blank?
-        @user.email_addresses.build
-      end
-    end
-
-    if params[:with] == 'sms'
-      if @user.phones.blank?
-        @user.phones.build
-      end
-    end
-
-    if @user.names.blank?
-      @user.names.build
-    end
-
+    profile = @user.profiles.new
+    profile.emails.new
+    profile.phones.new
+    profile.names.new
   end
 
   def update
-
-    begin
-
-      @user = current_user
-      @user.assign_attributes(user_params)
-
-      if @user.names.last.valid?
-        @user.names = [@user.names.last]
+    @user = current_user
+    respond_to do |format|
+      @user.code_1 = 'register'
+      @user.code_2 = params[:with]
+      if @user.update_attributes(user_params)
+        format.html { redirect_to user_path, :notice => "Регистрация завершена. Вы успешно вошли на сайт под своей учетной записью." }
       else
-        @user.names = [@user.names.first]
+        format.html { render :action => "edit" }
       end
-
-      @user.names.first.creation_reason = 'register'
-      @user.names.first.save
-
-      if params[:with] == 'sms'
-        if @user.phones.last.valid?
-          @user.phones = [@user.phones.last]
-        else
-          @user.phones = [@user.phones.first]
-        end
-
-        @user.phones.first.creation_reason = 'register'
-        @user.phones.first.save
-      end
-
-      if params[:with] == 'email'
-        if @user.email_addresses.last.valid?
-          @user.email_addresses = [@user.email_addresses.last]
-        else
-          @user.email_addresses = [@user.email_addresses.first]
-        end
-
-        @user.email_addresses.first.creation_reason = 'register'
-        @user.email_addresses.first.save
-      end
-
-      @user.password_required = true
-
-      respond_to do |format|
-        if @user.valid?
-          @user.role = 'user'
-          @user.save
-          format.html { redirect_to root_path, :notice => "Регистрация завершена. Вы вошли на сайт под своим аккаунтом." }
-        else
-          format.html { render :action => "edit" }
-        end
-      end
-    rescue ValidationError => e
-      render :edit
-      #redirect_to :back, :alert => e.message
     end
   end
 
   private
   
   def user_params
-    params.require(:user).permit(
-      { :names_attributes => [:name]},
-      { :email_addresses_attributes => [:email_address] },
-      { :phones_attributes => [:phone, :phone_type] },
+    params.require(:user).permit( { 
+      :profiles_attributes => [
+        { :names_attributes => [:name, :hidden_recreate] },
+        { :emails_attributes => [:value, :hidden_recreate] },
+        { :phones_attributes => [:value, :hidden_recreate] },
+      ] },
       :password,
       :password_confirmation
     )
