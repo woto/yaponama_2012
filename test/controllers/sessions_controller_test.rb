68d9351e38bf105b2e50dcc9c40b43124decef25
Ruslan Kornev <oganer@gmail.com>
2013-09-05 12:31:42 +0400

    Тест входа

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,19 +3,49 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "Проверка входа по номеру телефона" do
-    post :create, {login: '1111111111', password: '1111111111'}
-    assert_equal flash[:notice], "Вы успешно вошли."
+
+  test 'Контроллер не должен отдавать никаких странице пользователям с ролью не guest. Кроме выхода' do
+    skip
   end
 
-  test "Проверка входа по e-mail" do
-    post :create, {login: 'foo@example.com', password: '1111111111'}
-    assert_equal flash[:notice], 'Вы успешно вошли.'
+  test 'Форма входа с помощью номера телефона' do
+    get :new, {with: 'phone'}
+    assert_select "input[name='session[value]']"
+    assert_select  "input[name='session[password]']"
+    assert_select  "input[name='session[mobile]']"
   end
 
-  test 'Проверка ошибочного ввода пароля' do
-    post :create, {login: '1111111111', password: '1'}
-    assert_equal flash[:alert], 'Пара e-mail/телефон и пароль не найдены.'
+  test 'Форма входа с помощью e-mail' do
+    get :new, {with: 'email'}
+    assert_select "input[name='session[value]']"
+    assert_select  "input[name='session[password]']"
+  end
+
+  test 'Вход с помощью номера телефона. Поля не заполнены' do
+    post :create, with: 'phone', session: {value: '', password: ''}
+    assert_equal ["не может быть пустым"], assigns(:session).errors[:value]
+    assert_equal ["не может быть пустым"], assigns(:session).errors[:password]
+  end
+
+  test 'Вход с помощью e-mail. Поля не заполнены' do
+    post :create, with: 'email', session: {value: '', password: ''}
+    assert_equal ["не может быть пустым"], assigns(:session).errors[:value]
+    assert_equal ["не может быть пустым"], assigns(:session).errors[:password]
+  end
+
+  test 'Мобильный номер телефона указан в неверном формате.' do
+    post :create, with: 'phone', session: {value: '123', password: '123', mobile: true}
+    assert_equal ["имеет неверное значение"], assigns(:session).errors[:value]
+  end
+
+  test 'Городской номер телефона указан в произвольном формате.' do
+    post :create, with: 'phone', session: {value: '123', password: '123', mobile: false}
+    assert assigns(:session).errors[:value].blank?
+  end
+
+  test 'E-mail указан в неверном формате' do
+    post :create, with: 'email', session: {value: 'abc', password: ''}
+    assert_equal ["имеет неверное значение"], assigns(:session).errors[:value]
   end
 
   test "Если админ с правильно выставленным auth_token делаем delete :destroy. То auth_token должен сброситься и его должно редиректнуть" do
@@ -25,4 +55,19 @@ class SessionsControllerTest < ActionController::TestCase
     assert_equal response.code, '302'
   end
 
+  test 'Если стоит галка запомнить меня, то должны запомнить на X времени' do
+    skip
+  end
+
+  test 'Если не стоит галка запомнить, то забываем после закрытия браузера' do
+    skip
+  end
+
+  test 'Т.к. допустимо иметь несколько одинаковых phone или email, то вход должен осуществиться в соответствии с паролем учетной записи' do
+    skip
+  end
+
+  test 'Проверять with(?)' do
+    skip
+  end
 end
