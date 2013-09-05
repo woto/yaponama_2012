edac1a91c7f0d5ee07d0bc6f3b2d24b4cfdb6777
Ruslan Kornev <oganer@gmail.com>
2013-09-05 13:11:49 +0400

    Тесты Mailer'ов

---
+++ test/mailers/password_reset_test.rb
@@ -0,0 +1,12 @@
+#require 'test_helper'
+#
+#class NotificationsTest < ActionMailer::TestCase
+#  test "signup" do
+#    mail = Notifications.signup
+#    assert_equal "Signup", mail.subject
+#    assert_equal ["to@example.org"], mail.to
+#    assert_equal ["from@example.com"], mail.from
+#    assert_match "Hi", mail.body.encoded
+#  end
+#
+#end
--- test/mailers/receive_test.rb
+++ test/mailers/receive_test.rb
@@ -12,16 +12,18 @@ class ReceiveTest < ActionMailer::TestCase
 
   test "Если письмо приходит от неизвестного адреса, то должен создасться пользователь и привязанный к нему профиль с почтовым адресом и именем взятым из письма. А так же у этого пользователя создастся talk запись связанная с этим письмом." do
     mail = ReceiveMailer.receive(email_fixture('39.eml'))
-    assert_equal User.last.profiles.first.email_addresses.first.email_address, "oganer@gmail.com"
+    assert_equal User.last.profiles.first.emails.first.value, "oganer@gmail.com"
     assert_equal User.last.profiles.first.names.first.name, "Корнев Руслан"
     assert_equal User.last.talks.last.talkable.subject, "Тестирование вложений" 
   end
 
   test "Если письмо приходит от адреса, закрепленного за пользователем, то письмо попадает в talk к нему. Имя в соответствующем профиле не обновляется, т.к. такой адрес уже был" do
-    assert_difference(User.find(users(:first_user).id).talks) do
-      mail = ReceiveMailer.receive(email_fixture('40.eml'))
+
+    assert_no_difference "emails(:first_user).profile.names.count" do
+      assert_difference "users(:first_user).talks.count" do
+        mail = ReceiveMailer.receive(email_fixture('40.eml'))
+      end
     end
-    assert (EmailAddress.find(email_addresses(:first_user_email_address).id).profile.names).blank?
   end
 
 end
