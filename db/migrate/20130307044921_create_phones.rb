9609fab5bab7d346628f3d5b21cb9692a9cc6bfc
Ruslan Kornev <oganer@gmail.com>
2013-09-05 13:47:53 +0400

    Миграция Phone (phone -> value, phone_type -> mobile)

--- db/migrate/20130307044921_create_phones.rb
+++ db/migrate/20130307044921_create_phones.rb
@@ -1,8 +1,9 @@
 class CreatePhones < ActiveRecord::Migration
   def change
     create_table :phones do |t|
-      t.string :phone
-      t.string :phone_type
+      t.string :value
+      t.boolean :mobile
+      #t.string :phone_type
       t.references :profile, index: true
 
       t.timestamps
