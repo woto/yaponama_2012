cf6e0744b2cd177989cc3a6b081c8831ecf15d62
Ruslan Kornev <oganer@gmail.com>
2013-09-16 19:50:06 +0400

    В продолжение к предыдущему коммиту. Тестирование не вложенного placeholder

--- test/integration/text_input.rb
+++ test/integration/text_input.rb
@@ -2,15 +2,23 @@
 
 require 'test_helper'
 
-# TODO пока это тут, до момента пока окончательно не 
+# TODO пока это тут, до момента пока окончательно не
 # разберусь с helper'ами и их тестированием
 
 class TextInputTest < ActionDispatch::IntegrationTest
-  test 'У kpp автомобиля должен быть правильный placeholder' do
+  test 'Проверка функциональности вложенного в модель placeholder' do
+    Capybara.reset!
     auth('+7 (111) 111-11-11', '1111111111')
     visit '/user/cars/new'
     click_link 'Характеристики'
     assert_equal "Механика, Автомат, Вариатор и т.д.", find('#car_kpp')['placeholder']
+  end
+
+  test 'Проверка функциональности placeholder' do
+    Capybara.reset!
+    auth('+7 (111) 111-11-11', '1111111111')
+    visit '/user/postal_addresses/new'
+    assert_equal 'Номер квартиры или офиса', find('#postal_address_room')['placeholder']
   end
 end
 
