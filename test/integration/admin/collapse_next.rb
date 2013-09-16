5d73f74a4f1d37e6aa92c4fcf09c1e37e2c995ce
Ruslan Kornev <oganer@gmail.com>
2013-09-16 16:31:22 +0400

    Добавил тест для js collapse-next

---
+++ test/integration/admin/collapse_next.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+require 'test_helper'
+
+class Admin::CollapseNextTest < ActionDispatch::IntegrationTest
+
+  test 'Проверка работы collapse-next функционала' do
+    Capybara.reset!
+    auth('+7 (111) 111-11-11', '1111111111')
+    user = users(:first_user)
+    postal_address = postal_addresses(:first_user)
+    visit "/admin/users/#{user.id}/postal_addresses/#{postal_address.id}/edit"
+
+    # Вначале блок свернут
+    assert page.has_css?('[rel="acceptance-test"]', visible: false)
+
+    # Разворачиваем
+    find("[rel=collapse-next] span").click
+
+    # Потом соответственно развернут
+    assert page.has_css?('[rel="acceptance-test"]')
+  end
+end
