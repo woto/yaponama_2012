d62bfcf7d74715fb63409c6e472694839feff13d
Ruslan Kornev <oganer@gmail.com>
2013-09-19 23:56:16 +0400

    Мелочь. Тест - переименования Profileables -> Profiles и т.д.

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,7 +2,7 @@
 
 require 'test_helper'
 
-class ProfileablesControllerTest < ActionController::TestCase
+class ProfilesControllerTest < ActionController::TestCase
   #setup do
   #  @profile = profiles(:first_profile)
   #end
@@ -10,9 +10,9 @@ class ProfileablesControllerTest < ActionController::TestCase
   test 'Если покупатель хочет добавить новый профиль, то на странице нового профиля должны быть сразу добавлены для ввода имя и телефон.' do
     get 'new', { user_id: 1, resource_class: 'Profile' }
     assert_select '#profile_names_attributes_0_name'
-    assert_select '#profile_phones_attributes_0_phone'
+    assert_select '#profile_phones_attributes_0_value'
     assert_select '#profile_passports_attributes_0_passport', false
-    assert_select '#profile_email_addresses_attributes_0_email_address', false
+    assert_select '#profile_emails_attributes_0_value', false
   end
 
   test 'Если покупатель добавляет новый профиль, то кнопка удаления телефона должна отсутствовать' do
@@ -21,10 +21,9 @@ class ProfileablesControllerTest < ActionController::TestCase
   end
 
   test 'А вот при редактировании удалить первый номер телефона уже можно (т.е. например был добавлено два, а первый больше не рабочий. Иначе пришлось бы копировать второй телефон, его удалять и вставлять в первый)' do
-    @profile = profiles(:first_admin_profile)
+    @profile = profiles(:first_admin)
     get 'edit', { user_id: @profile.user.id, resource_class: 'Profile', id: @profile.id }
     assert_select '#profile_phones_attributes_0__destroy'
   end
-
 
 end
