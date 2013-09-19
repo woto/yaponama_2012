5230b5c6bf58f317ff1656ffba35e068bae1c881
Ruslan Kornev <oganer@gmail.com>
2013-09-19 19:45:55 +0400

    Регистрация. аналогично предыдущему, плюс старые проверенные изменения (аттрибуты, инициализация профиля и вложенных (names, phones и т.д.))

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -3,13 +3,14 @@
 class RegistersController < ApplicationController
 
   before_action :only_not_authenticated, :only => [:edit, :update, :show]
+  before_action { @meta_title = 'Регистрация' }
 
   def edit
     @user = current_user
-    profile = @user.profiles.new
-    profile.emails.new
-    profile.phones.new
-    profile.names.new
+    profile = @user.profiles.first_or_initialize
+    profile.emails.first_or_initialize
+    profile.phones.first_or_initialize
+    profile.names.first_or_initialize
   end
 
   def update
@@ -18,7 +19,7 @@ class RegistersController < ApplicationController
       @user.code_1 = 'register'
       @user.code_2 = params[:with]
       if @user.update_attributes(user_params)
-        format.html { redirect_to user_path, :notice => "Регистрация завершена. Вы успешно вошли на сайт под своей учетной записью." }
+        format.html { redirect_to user_path, :success => "Регистрация завершена. Вы успешно вошли на сайт под своей учетной записью." }
       else
         format.html { render :action => "edit" }
       end
@@ -28,11 +29,13 @@ class RegistersController < ApplicationController
   private
   
   def user_params
+    #params.require(:user).permit!
     params.require(:user).permit( { 
-      :profiles_attributes => [
-        { :names_attributes => [:name, :hidden_recreate] },
-        { :emails_attributes => [:value, :hidden_recreate] },
-        { :phones_attributes => [:value, :hidden_recreate] },
+      :profiles_attributes => [ 
+        :id,
+        { :names_attributes => [:id, :name, :hidden_recreate] },
+        { :emails_attributes => [:id, :value, :hidden_recreate] },
+        { :phones_attributes => [:id, :value, :hidden_recreate] },
       ] },
       :password,
       :password_confirmation
