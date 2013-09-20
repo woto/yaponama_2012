7983ec6e9a04f4c71834463db200f4ce8f9dbbf3
Ruslan Kornev <oganer@gmail.com>
2013-09-20 09:21:24 +0400

    Фиксирую concerns/confirmed, попробую отказаться от пары confirmed.

--- app/models/concerns/confirmed.rb
+++ app/models/concerns/confirmed.rb
@@ -1,24 +1,51 @@
+#encoding: utf-8
+
 module Confirmed
+  include Tokenable
   extend ActiveSupport::Concern
 
   included do
-    scope :confirmed, -> { 
+
+    attr_accessor :pin
+    attr_accessor :pin_required
+
+    validates :pin, presence: true, if: -> { pin_required }
+
+    validate if: -> { pin_required && errors.empty? } do
+      if pin != confirmation_token
+        errors.add(:pin, 'указан неверный PIN код.')
+      else
+        self.confirmation_token = ''
+        self.confirmed_by_user = true
+      end
+    end
+
+  end
+
+  module ClassMethods
+
+    def confirmed
       where(( arel_table[:confirmed_by_user].eq(true) ).or( arel_table[:confirmed_by_manager].eq(true) ))
-    }
+    end
+
+    def not_confirmed
+      where(( arel_table[:confirmed_by_user].eq(false) ).and( arel_table[:confirmed_by_manager].eq(false) ))
+    end
+
   end
 
   def confirmed?
     confirmed_by_user || confirmed_by_manager
   end
 
-  def confirmed_changed?
-    confirmed_by_user_changed? || confirmed_by_manager_changed?
+  def become_confirmed?
+    ( confirmed_by_user_was.blank? && confirmed_by_user.present? ) || (confirmed_by_manager_was.blank? && confirmed_by_manager.present? )
   end
 
   def reset_confirmed
     self.confirmed_by_user = false
     self.confirmed_by_manager = false
-    self.confirmation_token = SecureRandom.hex[0...4]
+    generate_token(:confirmation_token, :short)
   end
 
 end
