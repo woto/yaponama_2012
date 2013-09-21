56b51c72118b78ce9d037b75308512a33f139c43
Ruslan Kornev <oganer@gmail.com>
2013-09-21 05:07:25 +0400

    Мелочь. Тест. Переименования email_address -> email. Уже давно изменившийся способ указания номера телефона для входа, проверка транзакция и правильности выставления creation_reason

--- test/integration/profiles_test.rb
+++ test/integration/profiles_test.rb
@@ -10,11 +10,11 @@ class TransactionsTest < ActionDispatch::IntegrationTest
 
   test 'Если покупатель создает новый профиль из имени, телефона, эл. почты и паспорта, то в созданных транзакциях мы убеждаемся, что: У всех выставляется операция create, у всех выставляется правильно создатель и владелец причина создания profile. И дополнительно у всех дочерних правильно указан родительский профиль. Аналогично далее делаем проверку от лица администратора, изменяя все данные. И в конце удаляем от пользователя' do
     
-    authenticated_as '1231231231', '1231231231' do |sess|
+    authenticated_as '+7 (123) 123-12-31', '1231231231' do |sess|
 
       post user_profiles_path, { resource_class: 'Profile' }.merge(user_profile_attributes)
 
-      ['name', 'phone', 'email_address', 'passport', 'profile'].each do |profileable|
+      ['name', 'phone', 'email', 'passport', 'profile'].each do |profileable|
         instance_eval <<-CODE, __FILE__, __LINE__ + 1
 
           assert_equal 'create', #{profileable.camelize}Transaction.last.operation
@@ -23,21 +23,21 @@ class TransactionsTest < ActionDispatch::IntegrationTest
 
           if profileable != 'profile'
             assert_equal #{profileable.camelize}Transaction.last.profile_id_after, Profile.last.id
-            assert_equal assigns(:resource).#{profileable.pluralize}.first.creation_reason, 'profile'
+            assert_equal 'frontend', assigns(:resource).#{profileable.pluralize}.first.creation_reason 
           else
-            assert_equal assigns(:resource).creation_reason, 'profile'
+            assert_equal 'frontend', assigns(:resource).creation_reason
           end
 
         CODE
       end
     end
 
-    authenticated_as '1111111111', '1111111111' do |sess|
+    authenticated_as '+7 (111) 111-11-11', '1111111111' do |sess|
       request = { resource_class: 'Profile' }.merge(changed_user_profile_attributes)
 
       patch admin_user_profile_path(users(:first_user), Profile.last), request
 
-      ['name', 'phone', 'email_address', 'passport', 'profile'].each do |profileable|
+      ['name', 'phone', 'email', 'passport', 'profile'].each do |profileable|
 
         instance_eval <<-CODE, __FILE__, __LINE__ + 1
 
@@ -47,9 +47,11 @@ class TransactionsTest < ActionDispatch::IntegrationTest
 
           if profileable != 'profile'
             assert_equal #{profileable.camelize}Transaction.last.profile_id_after, Profile.last.id
-            assert_equal assigns(:resource).#{profileable.pluralize}.first.creation_reason, 'profile'
+            # TODO Кстати тут правильно по смыслу остался creation_reason 'frontend', т.к.
+            # Наверно нужно написать похожий тест, только создание от лица администратора
+            assert_equal 'frontend', assigns(:resource).#{profileable.pluralize}.first.creation_reason
           else
-            assert_equal assigns(:resource).creation_reason, 'profile'
+            assert_equal 'frontend', assigns(:resource).creation_reason
           end
 
         CODE
@@ -76,11 +78,11 @@ class TransactionsTest < ActionDispatch::IntegrationTest
     get transactions_admin_names_path
 
 
-    authenticated_as '1231231231', '1231231231' do |sess|
+    authenticated_as '+7 (123) 123-12-31', '1231231231' do |sess|
 
       delete user_profile_path(Profile.last), { resource_class: 'Profile' }
 
-      ['name', 'phone', 'email_address', 'passport', 'profile'].each do |profileable|
+      ['name', 'phone', 'email', 'passport', 'profile'].each do |profileable|
         instance_eval <<-CODE, __FILE__, __LINE__ + 1
 
           assert_equal 'destroy', #{profileable.camelize}Transaction.last.operation
