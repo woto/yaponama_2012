bdceff37c1768d5c9aa88ac6063a625a35c1339e
Ruslan Kornev <oganer@gmail.com>
2013-10-04 02:13:14 +0400

    И собственно зафиксирован AbstractGridable, который включается во все ProductsConcern, OrdersConcern, PagesConcern, ProfileablesConcern и т.д. Переделан set_grid, в котором создается анонимный AbstractGrid и вызывает adjust_columns!

--- app/controllers/concerns/abstract_gridable.rb
+++ app/controllers/concerns/abstract_gridable.rb
@@ -12,7 +12,20 @@ module AbstractGridable
     end
 
     def set_grid_class
-      raise 'set_grid_class'
+      @grid_class = Class.new(AbstractGrid)
+
+      @grid_class.instance_eval do
+        include ActiveModel::Validations
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "grid")
+        end
+      end
+
+      columns_hash = {}
+
+      adjust_columns!(columns_hash)
+
+      @grid_class.const_set("COLUMNS", columns_hash)
     end
 
     include GridConcern
