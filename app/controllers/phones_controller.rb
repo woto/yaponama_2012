ce442e839bc5a7e5023487c190f9bc16c7c0694f
Ruslan Kornev <oganer@gmail.com>
2013-10-03 20:45:55 +0400

    Phone

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -7,4 +7,20 @@ class PhonesController < ProfileablesController
     @grid.value_visible = '1'
   end
 
+  def adjust_columns!(columns_hash)
+    super
+    columns_hash['value'] = {
+      :type => :string,
+    }
+    columns_hash['mobile'] = {
+      :type => :boolean
+    }
+    columns_hash['confirmed'] = {
+      :type => :boolean
+    }
+    columns_hash['confirmation_datetime'] = {
+      :type => :date
+    }
+  end
+
 end
