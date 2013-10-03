17bdd7f2b78cfbbecda14772016f08ddb343c938
Ruslan Kornev <oganer@gmail.com>
2013-10-03 21:25:57 +0400

    PostalAddress

--- app/controllers/postal_addresses_controller.rb
+++ app/controllers/postal_addresses_controller.rb
@@ -2,6 +2,31 @@ class PostalAddressesController < ProfileablesController
 
   private
 
+  def adjust_columns!(columns_hash)
+    super
+    columns_hash['postcode'] = {
+      type: :string
+    }
+    columns_hash['region'] = {
+      type: :string
+    }
+    columns_hash['city'] = {
+      type: :string
+    }
+    columns_hash['street'] = {
+      type: :string
+    }
+    columns_hash['house'] = {
+      type: :string
+    }
+    columns_hash['stand_alone_house'] = {
+      type: :boolean
+    }
+    columns_hash['room'] = {
+      type: :string
+    }
+  end
+
   def set_preferable_columns
     super
     @grid.city_visible = '1'
