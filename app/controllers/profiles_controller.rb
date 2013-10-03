8d2d2d4b731ba1a3214532aa0173c82b683114c3
Ruslan Kornev <oganer@gmail.com>
2013-10-03 21:18:41 +0400

    Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,4 +14,20 @@ class ProfilesController < ProfileablesController
     @grid.cached_phones_visible = '1'
   end
 
+  def adjust_columns!(columns_hash)
+    super
+    columns_hash['cached_names'] = {
+      :type => :string,
+    }
+    columns_hash['cached_phones'] = {
+      :type => :string,
+    }
+    columns_hash['cached_emails'] = {
+      :type => :string,
+    }
+    columns_hash['cached_passports'] = {
+      :type => :string,
+    }
+  end
+
 end
