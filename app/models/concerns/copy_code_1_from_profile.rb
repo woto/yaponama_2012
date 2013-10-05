1f3270cadb2abd6ba6cf904b8efee4678289b0b3
Ruslan Kornev <oganer@gmail.com>
2013-10-05 17:15:13 +0400

    models/concern CopyCode1FromProfile

---
+++ app/models/concerns/copy_code_1_from_profile.rb
@@ -0,0 +1,10 @@
+module CopyCode_1FromProfile
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation if: -> {profile && profile.code_1.present?} do
+      self.code_1 = profile.code_1
+    end
+  end
+
+end
