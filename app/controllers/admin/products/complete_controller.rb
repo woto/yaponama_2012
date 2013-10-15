4161abe2031837ed141ff70414daabe515f23e68
Ruslan Kornev <oganer@gmail.com>
2013-10-15 06:40:13 +0400

    Comlete перед удалением

--- app/controllers/admin/products/complete_controller.rb
+++ app/controllers/admin/products/complete_controller.rb
@@ -8,6 +8,7 @@ class Admin::Products::CompleteController < ApplicationController # < Products::
     begin
 
       Rails.application.routes.recognize_path params[:return_path]
+      @items = @items.selected(@grid.item_ids)
       any_checked_validation
       products_all_statuses_validation ['stock', 'cancel']
 
