d96e506af5ffac0f2c546762b07089d5b857cf5c
Ruslan Kornev <oganer@gmail.com>
2013-10-25 22:41:16 +0400

    GridProfile

---
+++ app/controllers/concerns/grid_profile.rb
@@ -0,0 +1,39 @@
+#encoding: utf-8
+
+module GridProfile
+  extend ActiveSupport::Concern
+  include AbstractGridable
+
+  included do
+
+    private
+
+    def adjust_columns!(columns_hash)
+
+      c1(columns_hash) do
+        columns_hash['cached_names'] = {
+          :type => :string,
+        }
+        columns_hash['cached_phones'] = {
+          :type => :string,
+        }
+        columns_hash['cached_emails'] = {
+          :type => :string,
+        }
+        columns_hash['cached_passports'] = {
+          :type => :string,
+        }
+      end
+    end
+
+    def set_preferable_columns
+      super
+      @grid.visible_cached_names = '1'
+      @grid.visible_cached_phones = '1'
+      @grid.visible_cached_emails = '1'
+      @grid.visible_cached_passports = '1'
+    end
+
+  end
+
+end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,33 +1,10 @@
 class ProfilesController < ProfileablesController
+  include GridProfile
 
   def new
     super
     @resource.names.new
     @resource.phones.new(hide_remove_button_on_first_on_new: true, mobile: true)
-  end
-
-  private
-
-  def set_preferable_columns
-    super
-    @grid.cached_names_visible = '1'
-    @grid.cached_phones_visible = '1'
-  end
-
-  def adjust_columns!(columns_hash)
-    super
-    columns_hash['cached_names'] = {
-      :type => :string,
-    }
-    columns_hash['cached_phones'] = {
-      :type => :string,
-    }
-    columns_hash['cached_emails'] = {
-      :type => :string,
-    }
-    columns_hash['cached_passports'] = {
-      :type => :string,
-    }
   end
 
 end
