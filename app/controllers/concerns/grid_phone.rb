262cd18728fd353b5a56eaa0d6320179329ff933
Ruslan Kornev <oganer@gmail.com>
2013-10-25 22:24:18 +0400

    GridPhone и контроллер

---
+++ app/controllers/concerns/grid_phone.rb
@@ -0,0 +1,38 @@
+#encoding: utf-8
+
+module GridPhone
+
+  extend ActiveSupport::Concern
+  include AbstractGridable
+
+  included do
+
+    def adjust_columns!(columns_hash)
+
+      c1(columns_hash) do
+
+        columns_hash['value'] = {
+          :type => :string,
+        }
+        columns_hash['mobile'] = {
+          :type => :boolean
+        }
+        columns_hash['confirmed'] = {
+          :type => :boolean
+        }
+        columns_hash['confirmation_datetime'] = {
+          :type => :date
+        }
+
+      end
+
+    end
+
+    def set_preferable_columns
+      super
+      @grid.visible_value = '1'
+    end
+
+  end
+
+end
--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,26 +1,3 @@
 class PhonesController < ProfileablesController
-
-  private
-
-  def set_preferable_columns
-    super
-    @grid.value_visible = '1'
-  end
-
-  def adjust_columns!(columns_hash)
-    super
-    columns_hash['value'] = {
-      :type => :string,
-    }
-    columns_hash['mobile'] = {
-      :type => :boolean
-    }
-    columns_hash['confirmed'] = {
-      :type => :boolean
-    }
-    columns_hash['confirmation_datetime'] = {
-      :type => :date
-    }
-  end
-
+  include GridPhone
 end
