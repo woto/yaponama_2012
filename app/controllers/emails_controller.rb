354d1fea3302d653f373f523e8f59083ba0f96f2
Ruslan Kornev <oganer@gmail.com>
2013-10-25 22:21:38 +0400

    GridEmail и контроллер

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,24 +1,4 @@
 class EmailsController < ProfileablesController
-
-  private
-
-  def set_preferable_columns
-    super
-    @grid.value_visible = '1'
-  end
-
-  def adjust_columns!(columns_hash)
-    super
-    columns_hash['value'] = {
-      :type => :string,
-    }
-    columns_hash['confirmed'] = {
-      :type => :boolean
-    }
-    columns_hash['confirmation_datetime'] = {
-      :type => :date
-    }
-  end
-
+  include GridEmail
 end
 
