ac577d2848e1afa076fae4bce95c44cb3a43887a
Ruslan Kornev <oganer@gmail.com>
2013-10-27 00:41:14 +0400

    Обновил products_controller_test.rb в связи с тем, что обновился редирект после сохранения объекта на страницу просмотра.

--- test/controllers/admin/products_controller_test.rb
+++ test/controllers/admin/products_controller_test.rb
@@ -10,7 +10,7 @@ class Admin::ProductsControllerTest < ActionController::TestCase
 
     cookies['auth_token'] = somebodies(:first_admin).auth_token
     @user = somebodies(:first_admin)
-    return_path = '/user'
+    return_path = 'http://ya.ru'
     post :create, {:user_id => @user.id, :product => new_brand, :return_path => return_path}
 
     brand = Brand.last
@@ -20,12 +20,12 @@ class Admin::ProductsControllerTest < ActionController::TestCase
     product = @user.products.last
     assert_equal product, assigns(:resource), 'Добавленный товар не равен обрабатываемому @resource'
     assert_equal 'new', product.catalog_number, 'Добавленный товар имеет неверный каталожный номер'
-    assert_equal 'incart', product.status 'Добавленный товар имеет неверный статус'
+    assert_equal 'incart', product.status, 'Добавленный товар имеет неверный статус'
     assert_equal brand, product.brand, 'Ассоциация у добавленного товара parent_brand не равна созданной автоматически созданной модели brand'
     assert_equal 'new', product.cached_brand, 'Кешированное значение бренда у добавленного товара неверное'
     
     assert_response :redirect, 'Редирект не произошел'
-    assert_redirected_to return_path, 'Редирект произошел не неверный адрес'
+    assert_redirected_to admin_user_product_path(@user, product, :return_path => 'http://ya.ru'), 'Редирект произошел не неверный адрес'
 
   end
 
