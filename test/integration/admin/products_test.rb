3ca9d79cd21743fe370469f7801ef7d01e7b00bd
Ruslan Kornev <oganer@gmail.com>
2013-10-29 00:04:18 +0400

    Изменил немного только что написанные тесты, чтобы не облажаться если товаров будет много и они съедут на другую страницу

--- test/integration/admin/products_test.rb
+++ test/integration/admin/products_test.rb
@@ -5,9 +5,10 @@ require 'test_helper'
 class Admin::ProductsTest < ActionDispatch::IntegrationTest
 
   test 'Если у товара стоит сокрытие каталожного номера, то он показывается администратору со звездочкой' do
-    id = products(:first_admin_first_product).id
+    id = products(:anton_first).id
+    anton = somebodies(:anton)
     cookies['auth_token'] = somebodies(:first_admin).auth_token
-    get_via_redirect '/admin/products'
+    get_via_redirect "/admin/users/#{anton.id}/products"
     assert_select "#catalog_number_product_#{id}" do |elements|
       elements.each do |el|
         assert_match /2102/, el.to_s
@@ -17,10 +18,11 @@ class Admin::ProductsTest < ActionDispatch::IntegrationTest
   end
 
   test 'Если мы накладываем фильтр, то она отображается независимо от того, что каталожный номер сокрыт' do
-    id = products(:first_admin_first_product).id
+    id = products(:anton_first).id
+    anton = somebodies(:anton)
     cookies['auth_token'] = somebodies(:first_admin).auth_token
-    get_via_redirect '/admin/products'
-    post_via_redirect '/admin/products/filters/', "primary_key" => request.params['primary_key'], "filters" => 'ok', "grid[filter_catalog_number_like]" => '2102'
+    get_via_redirect "/admin/users/#{anton.id}/products"
+    post_via_redirect "/admin/users/#{anton.id}/products/filters/", "primary_key" => request.params['primary_key'], "filters" => 'ok', "grid[filter_catalog_number_like]" => '2102'
     assert_select "#product_#{id}", true
   end
 
--- test/integration/products_test.rb
+++ test/integration/products_test.rb
@@ -5,8 +5,8 @@ require 'test_helper'
 class ProductsTest < ActionDispatch::IntegrationTest
 
   test 'Если у товара стоит сокрытие каталожного номера, то показывается только звездочка' do
-    id = products(:first_admin_first_product).id
-    cookies['auth_token'] = somebodies(:first_admin).auth_token
+    id = products(:anton_first).id
+    cookies['auth_token'] = somebodies(:anton).auth_token
     get_via_redirect '/user/products'
     #assert_select "#catalog_number_product_#{id}", :text => '2102', count: 0
     assert_select "#catalog_number_product_#{id}" do |elements|
@@ -18,15 +18,15 @@ class ProductsTest < ActionDispatch::IntegrationTest
   end
 
   test 'Если у товара не стоит сокрытие каталожного номера, то мы прекрасного его видим' do
-    id = products(:first_admin_second_product).id
-    cookies['auth_token'] = somebodies(:first_admin).auth_token
+    id = products(:anton_second).id
+    cookies['auth_token'] = somebodies(:anton).auth_token
     get_via_redirect '/user/products'
     assert_select "#catalog_number_product_#{id}", :text => '3310'
   end
 
   test 'Если мы накладываем фильтр по кат. номеру, то даже при условии что он совпадает мы не увидим этой детали' do
-    id = products(:first_admin_first_product).id
-    cookies['auth_token'] = somebodies(:first_admin).auth_token
+    id = products(:anton_first).id
+    cookies['auth_token'] = somebodies(:anton).auth_token
     get_via_redirect '/user/products'
     post_via_redirect '/user/products/filters/', "primary_key" => request.params['primary_key'], "filters" => 'ok', "grid[filter_catalog_number_like]" => '2102'
     assert_select "#product_#{id}", false
