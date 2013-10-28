daefc9df915bf1fc2dd3bc987867d4d4f7fae639
Ruslan Kornev <oganer@gmail.com>
2013-10-28 22:34:07 +0400

    Обновил тесты по отображению кат.номеров с учетом отображения звездочки

--- test/integration/admin/products_test.rb
+++ test/integration/admin/products_test.rb
@@ -8,7 +8,12 @@ class Admin::ProductsTest < ActionDispatch::IntegrationTest
     id = products(:first_admin_first_product).id
     cookies['auth_token'] = somebodies(:first_admin).auth_token
     get_via_redirect '/admin/products'
-    assert_select "#catalog_number_product_#{id}", :text => '2102', count: 1
+    assert_select "#catalog_number_product_#{id}" do |elements|
+      elements.each do |el|
+        assert_match /2102/, el.to_s
+        assert_select "i[class=?]", /.*asterisk.*/
+      end
+    end
   end
 
 end
--- test/integration/products_test.rb
+++ test/integration/products_test.rb
@@ -8,14 +8,20 @@ class ProductsTest < ActionDispatch::IntegrationTest
     id = products(:first_admin_first_product).id
     cookies['auth_token'] = somebodies(:first_admin).auth_token
     get_via_redirect '/user/products'
-    assert_select "#catalog_number_product_#{id}", :text => '2102', count: 0
+    #assert_select "#catalog_number_product_#{id}", :text => '2102', count: 0
+    assert_select "#catalog_number_product_#{id}" do |elements|
+      elements.each do |el|
+        refute_match /2102/, el.to_s
+        assert_select "i[class=?]", /.*asterisk.*/
+      end
+    end
   end
 
   test 'Если у товара не стоит сокрытие каталожного номера, то мы прекрасного его видим' do
     id = products(:first_admin_second_product).id
     cookies['auth_token'] = somebodies(:first_admin).auth_token
     get_via_redirect '/user/products'
-    assert_select "#catalog_number_product_#{id}", :text => '3310', count: 1
+    assert_select "#catalog_number_product_#{id}", :text => '3310'
   end
 
 end
