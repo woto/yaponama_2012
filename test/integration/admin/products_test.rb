65417d5f1a0222c9342281bc0d36a201597500e2
Ruslan Kornev <oganer@gmail.com>
2013-10-28 23:22:30 +0400

    Еще пара тестов продуктов. Проверка попадания под фильтр сокрытого каталожного номера под администратором и пользователем

--- test/integration/admin/products_test.rb
+++ test/integration/admin/products_test.rb
@@ -16,5 +16,12 @@ class Admin::ProductsTest < ActionDispatch::IntegrationTest
     end
   end
 
-end
+  test 'Если мы накладываем фильтр, то она отображается независимо от того, что каталожный номер сокрыт' do
+    id = products(:first_admin_first_product).id
+    cookies['auth_token'] = somebodies(:first_admin).auth_token
+    get_via_redirect '/admin/products'
+    post_via_redirect '/admin/products/filters/', "primary_key" => request.params['primary_key'], "filters" => 'ok', "grid[filter_catalog_number_like]" => '2102'
+    assert_select "#product_#{id}", true
+  end
 
+end
--- test/integration/products_test.rb
+++ test/integration/products_test.rb
@@ -24,4 +24,13 @@ class ProductsTest < ActionDispatch::IntegrationTest
     assert_select "#catalog_number_product_#{id}", :text => '3310'
   end
 
+  test 'Если мы накладываем фильтр по кат. номеру, то даже при условии что он совпадает мы не увидим этой детали' do
+    id = products(:first_admin_first_product).id
+    cookies['auth_token'] = somebodies(:first_admin).auth_token
+    get_via_redirect '/user/products'
+    post_via_redirect '/user/products/filters/', "primary_key" => request.params['primary_key'], "filters" => 'ok', "grid[filter_catalog_number_like]" => '2102'
+    assert_select "#product_#{id}", false
+
+  end
+
 end
