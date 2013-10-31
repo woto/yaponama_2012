44ef07f9ebe1342c3d3c2da863b86bddaace5369
Ruslan Kornev <oganer@gmail.com>
2013-11-01 03:28:50 +0400

    Мелочь user -> somebody

--- db/seeds/50_suppliers.rb
+++ db/seeds/50_suppliers.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
 #rrda
-supplier = Supplier.new(SiteConfig.default_user_attributes)
+supplier = Supplier.new(SiteConfig.default_somebody_attributes)
 supplier.build_account
 supplier.code_1 = 'seed'
 supplier.save!
 
 #rrda
-supplier = Supplier.new(SiteConfig.default_user_attributes)
+supplier = Supplier.new(SiteConfig.default_somebody_attributes)
 supplier.build_account
 supplier.code_1 = 'seed'
 supplier.save!
