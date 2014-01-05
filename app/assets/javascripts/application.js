5899674ceaa3574d83b2f74f6508fdc7fa4cb982
Ruslan Kornev <oganer@gmail.com>
2014-01-06 01:25:43 +0400

    Вычищен holder и scroller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,8 +6,8 @@
 //= require bootstrap
 //= require turbolinks
 //= require cocoon
-//
-//
+
+
 // OTHER LIBRARIES
 //
 //= require purl/purl
@@ -18,9 +18,8 @@
 //= require underscore/underscore
 //= require moment/moment
 //= require jquery.maskedinput/dist/jquery.maskedinput.min
-//= require holder.js/holder
-//
-//
+
+
 // APPLICATION
 //
 //= require offers
@@ -31,27 +30,26 @@
 //= require phone_input
 //= require stand_alone_house
 //= require collapse_next
-//= require holder
 //= require companies
 //
 //= require catch_enter
-//
+
 //= require select2
 //= require brand_model
-//
+
 //= require jasmine_examples/Player
 //= require jasmine_examples/Song
-//
+
 //= require 'time_zone_checkbox'
 //= require 'vin_or_frame'
 //= require 'buy_button'
 //= require 'modal_form'
-//
-//
+
+
 //= require toolbar-checkboxes
-//
+
 // require jquery-dirtyforms/jquery.dirtyforms
-//
+
 //= require jquery.cachedScript
 //
 //= require ckeditor-helpers/extended_options
@@ -61,14 +59,14 @@
 //
 //= require deliveries/places
 //= require deliveries
-//
+
 //= require searches
-//
+
 //= require items_progress
 //= require 'auth'
 //= require 'tests'
 //= require 'jquery.editableHighlight'
-//
+
 //= require 'jquery-cookie/jquery.cookie'
 //= require 'sockjs-client/sockjs'
 //= require 'nanoScrollerJS/coffeescripts/jquery.nanoscroller'
@@ -79,7 +77,6 @@
 //= require talks/_talk
 //= letters
 //= legal
-//= require scroller
 //= require realtime
 //= require talk.js
 //= require notify_sound
