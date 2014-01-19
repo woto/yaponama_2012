686e725e01431053a1a6ff7826e563db6f8fe054
Ruslan Kornev <oganer@gmail.com>
2014-01-20 03:19:15 +0400

    Переделал twbs_submit в для modal footer (маленький размер и никаких пробелов) + кое-что по-мелочи

--- app/helpers/modal/footer.rb
+++ app/helpers/modal/footer.rb
@@ -1,7 +1,14 @@
 # encoding: utf-8
 #
 class Modal::Footer < AbstractBootstrap
-  def close
+  #def close
+  #  @template.content_tag :button, "Закрыть", class: 'btn btn-default', type: 'button', data: { dismiss: 'modal' }, 'aria-hidden' => 'true'
+  #end
+
+  def twbs_submit value=nil, options={}
+    options[:class] = [ 'btn btn-primary', options[:class] ].compact
+    @template.concat submit(value, options)
+    @template.concat '&nbsp;&nbsp;&nbsp;или&nbsp;&nbsp;&nbsp;'.html_safe
     @template.content_tag :button, "Закрыть", class: 'btn btn-default', type: 'button', data: { dismiss: 'modal' }, 'aria-hidden' => 'true'
   end
 end
