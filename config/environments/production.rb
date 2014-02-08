6e0cdc9fbd09b61c2131432d468d4a1d8ed33f85
Ruslan Kornev <oganer@gmail.com>
2014-02-08 18:51:39 +0400

    Оновил environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Yaponama2012::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
@@ -33,7 +33,7 @@ Yaponama2012::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.4'
+  config.assets.version = '1.5'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -62,7 +62,7 @@ Yaponama2012::Application.configure do
   config.assets.precompile += [
     "ltie9",
     "ckeditor/*",
-    "uploads.js",
+    "uploads.js"
   ]
 
   # Ignore bad email addresses and do not raise email delivery errors.
