092fe67b3c7f7bfbe2e11cfd584925e25fceea0a
Ruslan Kornev <oganer@gmail.com>
2014-03-10 14:26:48 +0400

    495px маленькая проблема в table_responsive

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Yaponama2012::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.12'
+  config.assets.version = '1.13'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
