32faa6444e0570d58972981f6111801e5709aedf
Ruslan Kornev <oganer@gmail.com>
2014-03-30 21:26:06 +0400

    asterisk

--- app/assets/javascripts/webrtc.js
+++ app/assets/javascripts/webrtc.js
@@ -5,7 +5,7 @@ $(document).on('click', '#call', function(event) {
   // Create our JsSIP instance and run it:
 
   var configuration = {
-    'ws_servers': "ws://avtorif.ru:8088/ws",
+    'ws_servers': "wss://avtorif.ru:8089/ws",
     'uri': "sip:1062@avtorif.ru",
     'password': "1062"
   };
@@ -54,7 +54,7 @@ $(document).on('click', '#call', function(event) {
     'mediaConstraints': {'audio': true, 'video': false}
   };
 
-  coolPhone.call('sip:1061@avtorif.ru', options);
+  coolPhone.call('sip:1000@avtorif.ru', options);
   //coolPhone.call('89169072788', options);
 
 });
