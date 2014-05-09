f721b5c94aef8b635c8b394d446eb9c0dd276787
Ruslan Kornev <oganer@gmail.com>
2014-05-09 04:24:32 +0400

    Добавил в виде вызов добавления задачи SpareCatalogWorker (отправляется на сервер прайсов каталожный номер, производитель и соответственно категория).

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Yaponama2012
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/extras #{config.root}/app/workers)
     #config.autoload_paths += %W(#{config.root}/delivery)
 
 
