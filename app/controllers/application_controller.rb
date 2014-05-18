36551a68f836ad275ec61dbc6ca1112b778b0336
Ruslan Kornev <oganer@gmail.com>
2014-05-18 13:14:37 +0400

    Выделил код из action_controller по обработке ошибок в ErrorHandling concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 #
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -28,7 +29,6 @@ class ApplicationController < ActionController::Base
 
   before_action :find_resource, except: [:new, :create, :index]
 
-  skip_before_action :find_resource, only: [:render_404, :render_500]
 
   # Потом вынести в concern transactionable?
   skip_before_action :find_resource, only: [:transactions]
@@ -48,13 +48,6 @@ class ApplicationController < ActionController::Base
 
   # Twitter Bootstrap 3
   add_flash_types :success, :info, :warning, :danger
-
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, :with => :render_500
-    rescue_from ActionController::RoutingError, :with => :render_404
-    rescue_from ActionController::UnknownController, :with => :render_404
-    rescue_from ActiveRecord::RecordNotFound, :with => :render_500
-  end
 
   rescue_from AuthenticationError, with: -> { redirect_to root_path, danger: "Возможно вы или кто-то другой входил на сайт под вашей учетной записью с другого компьютера. Вы можете отключить функцию автоматического выхода в Личном кабинете для возможности одновременной работы с разных компьютеров." }
   
@@ -122,24 +115,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # TODO потом детальнее посмотреть где и как используются @exception
-  def render_404(exception=nil)
-    @exception = exception
-    Rails.logger.error(exception)
-    respond_to do |format|
-      format.html { render 'error_404', :status => 404 }
-    end
-  end
-
-
-  def render_500(exception=nil)
-    @exception = exception
-    Rails.logger.error(exception)
-    respond_to do |format|
-      format.html { render 'error_500', :status => 500 }
-    end
-  end
-
+  include ErrorHandling
 
   private
 
