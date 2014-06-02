5fc3eb110bcb96c1d19109b28d35853db0390a5e
Ruslan Kornev <oganer@gmail.com>
2014-06-02 19:56:56 +0400

    Вернул замененный ранее nprogress с change на update

--- app/assets/javascripts/nprogress_init.js
+++ app/assets/javascripts/nprogress_init.js
@@ -1,5 +1,5 @@
 $(document).on('page:fetch',   function() { NProgress.start(); });
-$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:update',  function() { NProgress.done(); });
 $(document).on('page:restore', function() { NProgress.remove(); });
 
 $(document).on('submit', 'form[data-remote=true]', function() { NProgress.start(); });
