d02dabd68495aea0377098faba663466ce9faa6a
Ruslan Kornev <oganer@gmail.com>
2014-06-09 05:13:11 +0400

    Привел в порядок Gallery (контроллеры + вид)

--- app/controllers/admin/galleries_controller.rb
+++ app/controllers/admin/galleries_controller.rb
@@ -1,3 +1,12 @@
 class Admin::GalleriesController < GalleriesController
-end
+  include Admin::Admined
+  include GridGallery
 
+  skip_before_filter :set_grid, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  private
+
+  def set_resource_class
+    @resource_class = Gallery
+  end
+end
