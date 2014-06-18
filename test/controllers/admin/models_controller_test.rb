9571ef0959a7bc97b7d9734d675a02725933348c
Ruslan Kornev <oganer@gmail.com>
2014-06-18 08:21:00 +0400

    Мелочь. Исправлен тест.

--- test/controllers/admin/models_controller_test.rb
+++ test/controllers/admin/models_controller_test.rb
@@ -5,6 +5,8 @@ require 'test_helper'
 class Admin::ModelsControllerTest < ActionController::TestCase
   
   test 'Тестируем работу поиска, использующегося в выпадающем списке' do
+    first_admin = somebodies(:first_admin)
+    cookies['auth_token'] = first_admin.auth_token
     xhr :get, :search, {:format => :json}
     assert_response :success
   end
