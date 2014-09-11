2f1f84abf07cd1b51e427b998da3b3009cf6636c
Ruslan Kornev <oganer@gmail.com>
2014-09-11 18:59:31 +0600

    Добавил еще правил редиректа с старых searches и info

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -2,7 +2,9 @@ Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) 
   r301 %r{^/(.*)/$}, '/$1'
   r301 %r{^/ru/parts/search/\?text=(.+)}, '/user/products/new?catalog_number=$1'
   r301 %r{^/ru/parts/search/(.+)}, '/user/products/new?catalog_number=$1'
-  r301 %r{^/searches/(.+)/(.+)}, '/user/products/new?catalog_number=$1'
+  r301 %r{^/info/(.+)/(.+)}, '/user/products/new?catalog_number=$1'
+  r301 %r{^/searches/(.+?)/.*}, '/user/products/new?catalog_number=$1'
+  r301 %r{^/searches/(.+)\?}, '/user/products/new?catalog_number=$1'
   r301 %r{^/searches/(.+)}, '/user/products/new?catalog_number=$1'
   r301 %r{.*}, Proc.new {|path, rack_env| "http://www.#{rack_env['SERVER_NAME']}#{path}"}, :if => Proc.new {|rack_env|
     (rack_env['SERVER_NAME'][0...3] != 'www' && ENV['RACK_ENV'] == "production") &&
