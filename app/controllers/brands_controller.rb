57237480fb990d6695c59f57de7859345098d711
Ruslan Kornev <oganer@gmail.com>
2014-10-05 04:05:19 +0600

    Собственно сам обновленный brands_controller.rb с поиском по ассоциациям и учетом is_brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -8,13 +8,20 @@ class BrandsController < ApplicationController
   def search
     brand_t = Brand.arel_table
 
-    @resources = Brand
+    @resources = Brand.where(brand_id: nil)
 
     if params[:name].present?
-      @resources = Brand.where(brand_t[:name].matches("#{params[:name]}%"))
+      b = Arel::Table.new Brand.reflect_on_association(:brands)
+      b.table_alias = 'brands_brands'
+      @resources = @resources.where(brand_t[:name]. matches("#{params[:name]}%")
+                                    .or(b[:name].matches("#{params[:name]}%")))
+        .includes(:brands)
+        .references(:brands_brands)
     end
 
-    @resources = @resources.where(is_brand: true)
+    if params[:is_brand]
+      @resources = @resources.where(is_brand: true)
+    end
 
     @resources = @resources.order(:name).page params[:page]
 
--- rake/pg_restore.rb
+++ rake/pg_restore.rb
@@ -1,10 +1,10 @@
-class PgDump
-  def self.pg_dump
+class PgRestore
+  def self.pg_restore
 	  ActiveRecord::Base.establish_connection
     table_names = %w(bots brands models generations modifications faqs spare_catalogs spare_infos)
     table_names.each do |table_name|
       table_path = File.join(Rails.root, 'data', "#{table_name}.csv")
-      ActiveRecord::Base.connection.execute("COPY (SELECT * FROM #{table_name}) TO '#{table_path}' WITH CSV HEADER")
+      ActiveRecord::Base.connection.execute("COPY #{table_name} FROM '#{table_name}.csv' WITH CSV HEADER")
     end
   end
 end
