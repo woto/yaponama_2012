d9f7723e29ce00799f5941b2fc1437079ad90339
Ruslan Kornev <oganer@gmail.com>
2014-10-05 03:23:28 +0400

    В дамп добавил сортировку по ID

--- rake/pg_dump.rb
+++ rake/pg_dump.rb
@@ -4,7 +4,7 @@ class PgDump
     table_names = %w(bots brands models generations modifications faqs spare_catalogs spare_infos)
     table_names.each do |table_name|
       table_path = File.join(Rails.root, 'data', "#{table_name}.csv")
-      ActiveRecord::Base.connection.execute("COPY (SELECT * FROM #{table_name}) TO '#{table_path}' WITH CSV HEADER")
+      ActiveRecord::Base.connection.execute("COPY (SELECT * FROM #{table_name} ORDER BY ID) TO '#{table_path}' WITH CSV HEADER")
     end
   end
 end
