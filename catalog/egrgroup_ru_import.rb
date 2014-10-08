8260de25334af66f6d751bd0d8a86e4ea74112ae
Ruslan Kornev <oganer@gmail.com>
2014-10-08 21:35:38 +0600

    Мелочь

--- catalog/egrgroup_ru_import.rb
+++ catalog/egrgroup_ru_import.rb
@@ -7,41 +7,41 @@ class EgrgroupRuImport
 
         if !row[1].nil? && !row[2].nil? && !row[3].nil? && !row[4].nil? && !row[5].nil? && !row[6].nil? && !row[7].nil?
 
-        c9 = row[1]
-        b9 = row[3]
-        r9 = '0'
-        request_emex = '&ext_ws=0'
-        cached = '&cached=0'
+          c9 = row[1]
+          b9 = row[3]
+          r9 = '0'
+          request_emex = '&ext_ws=0'
+          cached = '&cached=0'
 
-        #binding.pry
-        price_request_url = "http://#{CONFIG.price['host']}:#{CONFIG.price['port']}/prices/search?catalog_number=#{CGI::escape(c9)}&manufacturer=#{CGI::escape(b9 || '')}&replacements=#{r9}#{request_emex}&format=json&for_site=1#{cached}"
+          #binding.pry
+          price_request_url = "http://#{CONFIG.price['host']}:#{CONFIG.price['port']}/prices/search?catalog_number=#{CGI::escape(c9)}&manufacturer=#{CGI::escape(b9 || '')}&replacements=#{r9}#{request_emex}&format=json&for_site=1#{cached}"
 
-        parsed_price_request_url = URI.parse(price_request_url)
-        resp = Net::HTTP.get_response(parsed_price_request_url)
-        @parsed_json = ActiveSupport::JSON.decode(resp.body)
-        #binding.pry
-        if @parsed_json["result_prices"].present?
+          parsed_price_request_url = URI.parse(price_request_url)
+          resp = Net::HTTP.get_response(parsed_price_request_url)
+          @parsed_json = ActiveSupport::JSON.decode(resp.body)
+          #binding.pry
+          if @parsed_json["result_prices"].present?
 
-          brand = BrandMate.find_or_create_canonical(row[3])
-          model = Model.find_or_create_by(name: row[4], brand: brand)
-          generation = Generation.find_or_create_by(name: row[2], model: model)
+            brand = BrandMate.find_or_create_canonical(row[3])
+            model = Model.find_or_create_by(name: row[4], brand: brand)
+            generation = Generation.find_or_create_by(name: row[2], model: model)
 
-          manufacturer = Brand.find_or_create_by(name: row[5])
-          spare_catalog = SpareCatalog.find_or_create_by(name: row[6].mb_chars.upcase, content: row[6].mb_chars.upcase)
-          spare_info = SpareInfo.find_or_create_by(catalog_number: row[1].gsub('-', ''), brand: manufacturer, spare_catalog: spare_catalog, content: row[7])
+            manufacturer = Brand.find_or_create_by(name: row[5])
+            spare_catalog = SpareCatalog.find_or_create_by(name: row[6].mb_chars.upcase, content: row[6].mb_chars.upcase)
+            spare_info = SpareInfo.find_or_create_by(catalog_number: row[1].gsub('-', ''), brand: manufacturer, spare_catalog: spare_catalog, content: row[7])
 
-          s = SpareApplicability.new(
-            spare_info: spare_info,
-            brand: brand,
-            model: model,
-            generation: generation
-          )
+            s = SpareApplicability.new(
+              spare_info: spare_info,
+              brand: brand,
+              model: model,
+              generation: generation
+            )
 
-          unless s.save
-            puts row.to_s
-            #binding.pry
-         end
-        end
+            unless s.save
+              puts row.to_s
+              #binding.pry
+           end
+          end
         end
       end
     end
