3103638ed11792935f594dd4247a7f638d5c0931
Ruslan Kornev <oganer@gmail.com>
2014-10-30 18:26:56 +0500

    Сделал редирект в rack_rewrite на поиск замен без производителя

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+  r301 %r{^/user/products/new\?catalog_number=(.*)&manufacturer=(.*)&replacements=1}, '/user/products/new?catalog_number=$1&replacements=1'
   r301 %r{^/(.*)/$}, '/$1'
   r301 %r{^/ru/parts/search/\?text=(.+)}, '/user/products/new?catalog_number=$1'
   r301 %r{^/ru/parts/search/(.+)}, '/user/products/new?catalog_number=$1'
