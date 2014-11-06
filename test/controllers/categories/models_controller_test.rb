e8ee4ce01f5450d3649f7e4e4e39f910d60d4608
Ruslan Kornev <oganer@gmail.com>
2014-11-06 06:52:00 +0500

    Категории -> Запчасти. + Добавил детальку 2103 (MS) в прайс и spare_infos

--- test/controllers/categories/models_controller_test.rb
+++ test/controllers/categories/models_controller_test.rb
@@ -5,7 +5,7 @@ class Categories::ModelsControllerTest < ActionController::TestCase
   test 'Быстрый тест на содержание страницы 348934' do
     get :show, id: models(:galant), category_id: spare_catalogs(:pylnik_rulevoi_reiki)
     assert_select '.page-header', 'ПЫЛЬНИК РУЛЕВОЙ РЕЙКИ MITSUBISHI Galant (рус. Галант)'
-    assert_select '.breadcrumb', /^Категории\s+ПЫЛЬНИК РУЛЕВОЙ РЕЙКИ\s+MITSUBISHI\s+Galant$/m
+    assert_select '.breadcrumb', /^Запчасти\s+ПЫЛЬНИК РУЛЕВОЙ РЕЙКИ\s+MITSUBISHI\s+Galant$/m
     assert_select '#best-parts-list', '3310 (INFINITI) ПЫЛЬНИК РУЛЕВОЙ РЕЙКИ'
   end
 
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,7 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   test 'Быстрый тест на содержание страницы 23498234' do
     get :index
-    assert_select '.page-header', 'Категории запчастей'
+    assert_select '.page-header', 'Запчасти'
     assert_select '#categories-brands', /БАМПЕР ЗАДНИЙ\s+ПЫЛЬНИК РУЛЕВОЙ РЕЙКИ\s+РУЛЕВАЯ ТЯГА\s+САЙЛЕНТБЛОК/ 
   end
 
