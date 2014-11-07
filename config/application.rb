65fcd99d877745ae62fc0bae404fc31c994e352e
Ruslan Kornev <oganer@gmail.com>
2014-11-07 21:01:09 +0500

    config.autoload_paths += lib

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Yaponama2012
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(
+      #{config.root}/lib
       #{config.root}/app/workers
       #{config.root}/rake
       #{config.root}/catalog)
