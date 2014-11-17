de916108f09cd52d27e87257d2d0acf80c7dfa3e
Ruslan Kornev <oganer@gmail.com>
2014-11-17 09:14:16 +0500

    Обновил скрипт генерации sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'sitemap_generator'
-require 'net/http'
-require 'csv'
+#require 'net/http'
+#require 'csv'
 
-SitemapGenerator::Sitemap.default_host = "http://#{SiteConfig.site_address}/"
+SitemapGenerator::Sitemap.default_host = "http://www.#{Rails.application.config_for('application/site')['host']}/"
 
 SitemapGenerator::Sitemap.create do
 
@@ -12,33 +12,12 @@ SitemapGenerator::Sitemap.create do
     sitemap.sitemaps_namer.next
   end
 
-  #add '/searches', :changefreq => 'daily'
-  #add '/contacts', :changefreq => 'weekly'
-
   Page.all.each do |page|
     add "/#{page.path}", :changefreq => 'monthly'
   end
 
-
-  # Прайсы
-  
-  Net::HTTP.start(SiteConfig.price_address, SiteConfig.price_port) do |http|
-    prices_urls = [
-      '/suppliers/156/price_settings/263/download',
-      '/suppliers/156/price_settings/264/download',
-      '/suppliers/157/price_settings/265/download',
-      '/suppliers/67/price_settings/106/download',
-      #'/suppliers/67/price_settings/139/download',
-      #'/suppliers/67/price_settings/135/download',
-    ]
-
-    prices_urls.each do |price_url|
-      resp = http.get(price_url)
-      CSV.parse(resp.body) do |row|
-        add "/user/products/new?catalog_number=#{row[1]}", :changefreq => 'weekly'
-      end
-    end
-
+  SpareInfo.all.each do |spare_info|
+    add "/user/products/new?catalog_number=#{spare_info.catalog_number}", :changefreq => 'weekly'
   end
 
 end
