3665317a60054928905cd9ef0d9653ea17b93c4d
Ruslan Kornev <oganer@gmail.com>
2014-12-19 22:39:56 +0500

    В cars_date расширен диапазон дат и добавлен пустой элемент

--- app/inputs/cars_date_input.rb
+++ app/inputs/cars_date_input.rb
@@ -1,7 +1,7 @@
 class CarsDateInput < SimpleForm::Inputs::Base
   def input(wrapper_options = nil)
     template.content_tag :div do
-      @builder.date_select attribute_name, {add_month_numbers: true, discard_day: true, start_year: 1995, end_year: Date.today.year}, {class: 'form-control datetime'}
+      @builder.date_select attribute_name, {add_month_numbers: true, discard_day: true, start_year: 1990, end_year: Date.today.year, include_blank: true}, {class: 'form-control datetime'}
     end
   end
 end
