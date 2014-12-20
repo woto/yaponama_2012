eda507404ae9144344603df84cb193c8905800c7
Ruslan Kornev <oganer@gmail.com>
2014-12-20 16:59:34 +0500

    spare_catalog у spare_info сбрасывается, если не удалось по токенам его определить

--- app/jobs/spare_catalog_job.rb
+++ app/jobs/spare_catalog_job.rb
@@ -2,8 +2,9 @@ class SpareCatalogJob < ActiveJob::Base
   queue_as :default
 
   def perform(catalog_number_string, brand, spare_catalog)
+    spare_catalog =  PriceMate.spare_catalog if spare_catalog.nil?
     if si = SpareInfo.find_by(catalog_number: catalog_number_string, brand: brand)
-      si.update(spare_catalog: spare_catalog)
+      si.update!(spare_catalog: spare_catalog)
     end
   end
 end
