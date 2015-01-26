b6e2e0e0851fff958fc1de00b36ef01ef4addf0f
Ruslan Kornev <oganer@gmail.com>
2015-01-26 23:10:10 +0500

    Страница Каталоги. Заменил @brands на @spare_applicabilities. Теперь бренды отображаются не списком, а иконками, с помощь brands_decorator.

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -2,6 +2,6 @@ class CatalogsController < ApplicationController
   skip_before_action :find_resource
 
   def show
-    @brands = SpareApplicability.by
+    @spare_applicabilities = SpareApplicability.by
   end
 end
