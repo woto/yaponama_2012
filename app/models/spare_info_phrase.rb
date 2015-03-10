1a32363075e98989e7aaf21432a8ad7351f52591
Ruslan Kornev <oganer@gmail.com>
2015-03-10 09:02:56 +0300

    Модель SpareInfoPhrase (необходима для работы как минимум 1-го предыдущего коммита)

---
+++ app/models/spare_info_phrase.rb
@@ -0,0 +1,19 @@
+class SpareInfoPhrase < ActiveRecord::Base
+  belongs_to :spare_info
+
+  validates :catalog_number, :presence => true, uniqueness:  { case_sensitive: false, :scope => :spare_info_id }
+
+  ransacker :catalog_number_string do
+    Arel.sql('length(catalog_number)')
+  end
+
+  before_save do
+    self.phrase = catalog_number.
+      gsub(/[^[:alnum:]]/i, ' ').
+      gsub(/[[:space:]]+/, ' ').
+      strip.
+      gsub(' ', ' !').
+      prepend('!')
+  end
+
+end
