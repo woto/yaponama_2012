17bba359d91315055ec6d6e501b2698990663e9b
Ruslan Kornev <oganer@gmail.com>
2015-04-09 09:31:11 +0500

    Ввел понятие host_port в конфигах, содержащий склееные значения. Прикрутил комментарии Discourse, надо поэкспериментировать. Обернул счетчики в grid

--- config/initializers/discourse.rb
+++ config/initializers/discourse.rb
@@ -1,4 +1,4 @@
-$discourse = DiscourseApi::Client.new(Rails.application.config_for('application/discourse')['url'])
+$discourse = DiscourseApi::Client.new('http://' + Rails.application.config_for('application/discourse')['host_port'])
 $discourse.api_username = Rails.application.config_for('application/discourse')['api_username']
 $discourse.api_key = Rails.application.config_for('application/discourse')['api_key']
 
