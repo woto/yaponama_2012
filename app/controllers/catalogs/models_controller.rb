5ecd65d84cccbafc95a18fbb33137d1c84e9257a
Ruslan Kornev <oganer@gmail.com>
2015-04-20 20:45:06 +0500

    /catalogs/models/id

--- app/controllers/catalogs/models_controller.rb
+++ app/controllers/catalogs/models_controller.rb
@@ -1,12 +1,20 @@
 class Catalogs::ModelsController < ApplicationController
 
+  def show
+    @spare_catalogs = SpareCatalog.
+      joins(:spare_infos => :spare_applicabilities).
+      where(:spare_applicabilities => {:model_id => params[:id].to_i}).
+      select("spare_catalogs.id, spare_catalogs.name, count(spare_infos.id) as count").
+      order("spare_catalogs.name").
+      group("spare_catalogs.id")
+  end
+
   private
+ 
 
   def find_resource
     @model = Model.find(params[:id])
     @brand = @model.brand
-
-    @spare_infos = SpareInfo.by_model(params[:id]).by
   end
 
 end
