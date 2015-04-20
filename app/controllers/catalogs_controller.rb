7f62d1f49c015822521db85895ffbe258ba71582
Ruslan Kornev <oganer@gmail.com>
2015-04-20 20:33:33 +0500

    /brands

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -2,6 +2,11 @@ class CatalogsController < ApplicationController
   skip_before_action :find_resource
 
   def show
-    @spare_applicabilities = SpareApplicability.by
+    @brands = Brand.
+      joins(:spare_applicabilities).
+      order("brands.name").
+      where("spare_applicabilities.brand_id IS NOT NULL").
+      select("brands.id, brands.name, brands.image, count(spare_applicabilities.id) as count").
+      group("brands.id")
   end
 end
