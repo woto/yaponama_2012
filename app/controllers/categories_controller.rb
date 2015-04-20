a9bc851cb0ed4e6696e94f9ccf5b56c19881d65d
Ruslan Kornev <oganer@gmail.com>
2015-04-20 20:26:11 +0500

    Categories index и show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,23 @@
 class CategoriesController < ApplicationController
 
   def index
-    @spare_infos = SpareInfo.by
+    @q = SpareCatalog.search(params[:q])
+    @spare_catalogs = @q.result(distinct: true)
+    @spare_catalogs = @spare_catalogs.
+      joins(:spare_infos => :spare_applicabilities).
+      select("spare_catalogs.id, spare_catalogs.name, count(spare_infos.id) as count").
+      order("spare_catalogs.name").
+      group("spare_catalogs.id")
   end
 
   def show
-    @spare_applicabilities = SpareApplicability.by.by_category(params[:id])
+    @brands = Brand.
+      joins(:spare_applicabilities).
+      order("brands.name").
+      where("spare_applicabilities.brand_id IS NOT NULL").
+      select("brands.id, brands.name, count(spare_applicabilities.id) as count").
+      group("brands.id").
+      by_category(params[:id])
 
     @q = SpareInfo.search(params[:q])
     @spare_infos = @q.result(distinct: true)
@@ -18,7 +30,7 @@ class CategoriesController < ApplicationController
   def find_resource
     @resource = SpareCatalog.find(params[:id])
     if @resource.opt
-      @resource_class = "Opts::#{@resource.opt.camelize}".constantize
+      @opt_class = "Opts::#{@resource.opt.camelize}".constantize
     end
   end
 
