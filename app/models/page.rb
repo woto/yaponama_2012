e5cf8f379efaba47e771cd1c92389c9f190faa8c
Ruslan Kornev <oganer@gmail.com>
2015-04-26 16:00:06 +0500

    Выпилил Page. Hola Ckpages!

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,36 +1,3 @@
-# encoding: utf-8
-#
 class Page < ActiveRecord::Base
-  include BelongsToCreator
-  include Transactionable
-  include Selectable
-  include Code_1AttrAccessorAndValidation
-  include SetCreationReasonBasedOnCode_1
-
-  # TODO разобраться почему это вызываю дважды
-  before_save :normalize
-  before_validation :normalize
-
-  validates :path, presence: true, uniqueness: {case_sensitive: false}
-  validates :redirect_url, format: { with: /\A\/|http:\/\/|https:\/\// }, allow_nil: true, allow_blank: true
-
-  has_and_belongs_to_many :uploads
-
-  def normalize
-
-    self.path = self.path.gsub(/\A\/+/, '')
-    self.path = self.path.gsub(/\/+\z/, '')
-    self.path = self.path.gsub(/\A\s+/, '')
-    self.path = self.path.gsub(/\s+\z/, '')
-
-    if self.path.length <= 0
-      errors.add(:path, "не может быть пустым.")
-      return false
-    end
-  end
-
-  def to_label
-    path
-  end
-
+  # TODO Когда мигрирую на ckpages, то удалить.
 end
--- app/models/page_transaction.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-#
-class PageTransaction < ActiveRecord::Base
-  include BelongsToCreator
-  include BelongsToSomebody
-end
