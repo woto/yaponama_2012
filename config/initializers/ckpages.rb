f3296ee4e5cec5738bcb47e3c1f4bada8eca9a54
Ruslan Kornev <oganer@gmail.com>
2015-05-13 20:59:31 +0500

    Закрыл от доступа ckpages

--- config/initializers/ckpages.rb
+++ config/initializers/ckpages.rb
@@ -1,2 +1,9 @@
 Ckpages.enabled = Rails.application.config_for('application/ckpages')['enabled']
 Ckpages.root = Rails.application.config_for('application/ckpages')['root']
+
+
+Ckpages::Engine.configure do |config|
+  config.middleware.use Rack::Auth::Basic do |username, password|
+    username == 'woto' && password == 'woto'
+  end
+end
