fc72b7a76b3ae39ab8bb0fa7072ba34433558806
Ruslan Kornev <oganer@gmail.com>
2015-06-08 01:50:26 +0300

    Доработка rake/download_warehouses.rb в связи с тем, что теперь SpareInfo не может существовать без единого SpareInfoPhrase

--- rake/download_warehouse.rb
+++ rake/download_warehouse.rb
@@ -13,8 +13,21 @@ class DownloadWarehouse
             brand: BrandMate.find_or_create_conglomerate(row[3]),
           )
           spare_info.assign_attributes(:spare_catalog => Defaults.spare_catalog) unless spare_info.spare_catalog
-          puts row unless spare_info.save
-          puts row unless place.warehouses.create(:spare_info => spare_info, :count => row[5], :price => row[6].to_i).persisted?
+
+          unless spare_info.spare_info_phrase
+            spare_info.spare_info_phrases.new(catalog_number: PriceMate.catalog_number(row[1]), primary: true)
+          end
+
+          unless spare_info.save
+            puts row
+            puts spare_info.errors.full_messages
+          end
+
+          unless place.warehouses.create(:spare_info => spare_info, :count => row[5], :price => row[6].to_i).persisted?
+            puts row
+            puts place.errors.full_messages
+          end
+
         end
       end
     end
