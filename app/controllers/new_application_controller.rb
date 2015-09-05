5df9b9f7b7440b0b0c2d411f194f74dd11c70e76
Ruslan Kornev <oganer@gmail.com>
2015-09-05 09:49:10 +0500

    Дохера чего удалил

--- app/controllers/new_application_controller.rb
+++
@@ -1,2 +0,0 @@
-class NewApplicationController < ActionController::Base
-end
--- app/models/passport_transaction.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-#
-class PassportTransaction < ActiveRecord::Base
-  include BelongsToCreator
-  include BelongsToSomebody
-end
