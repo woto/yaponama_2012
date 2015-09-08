53e7dc616f88eb7f2f57a70f823d58181a972f25
Ruslan Kornev <oganer@gmail.com>
2015-09-08 16:12:14 +0500

    1. rm cashes 2. rm previews

--- test/mailers/previews/seller_notifier_preview.rb
+++ test/mailers/previews/seller_notifier_preview.rb
@@ -1,12 +1,4 @@
 class SellerNotifierPreview < ActionMailer::Preview
-  def payment
-    SellerNotifierMailer.payment(Payment.last)
-  end
-
-  def incart
-    SellerNotifierMailer.incart(Product.last)
-  end
-
   def callback
     SellerNotifierMailer.callback(Callback.last)
   end
