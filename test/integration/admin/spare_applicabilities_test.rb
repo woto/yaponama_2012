926ad232616db5dc2c6f01807479ad428200ba7a
Ruslan Kornev <oganer@gmail.com>
2015-09-15 04:07:43 +0500

    .

--- test/integration/admin/spare_applicabilities_test.rb
+++ test/integration/admin/spare_applicabilities_test.rb
@@ -2,11 +2,22 @@ require 'test_helper'
 
 class Admin::SpareApplicabilitiesTest < ActionDispatch::IntegrationTest
 
-  test 'Щелкаем на выпадающем списке "Родительский производитель". Должны быть и неоригиналы' do
+  def setup
     Capybara.reset!
-    auth('+7 (111) 111-11-11', '1111111111')
+  end
+
+  test 'Проверяем работу автозаполнения по каталожному номеру' do
+    visit new_admin_spare_applicability_path
+    execute_script %q($("input[rel='select2-spare_info']").select2('open'))
+    find("input.select2-input").set('2102')
+    execute_script %q($("input.select2-input").trigger('change'))
+    assert has_text? "2102 (NISSAN)"
+  end
+
+  test 'Щелкаем на выпадающем списке. Неоригиналы видимы быть не должны' do
     visit "/admin/spare_applicabilities/#{spare_applicabilities(:sa_1).id}/edit"
     execute_script %q($("input[rel='select2-brand']").select2('open'))
+    execute_script %q($("input.select2-input").trigger('change'))
     assert has_text? "ACURA"
     assert has_no_text? "A.B.S."
   end
--- test/integration/admin/spare_infos_test.rb
+++ test/integration/admin/spare_infos_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class Admin::SpareInfosTest < ActionDispatch::IntegrationTest
 
+  def setup
+    Capybara.reset!
+  end
+
   test 'Щелкаем на выпадающем списке "Родительский производитель". Должны быть и неоригиналы' do
     Capybara.reset!
-    auth('+7 (111) 111-11-11', '1111111111')
-    visit "/admin/spare_infos/#{spare_infos(:toyota_3310).id}/edit"
+    visit "/admin/spare_infos/new"
     execute_script %q($("input[rel='select2-brand']").select2('open'))
     assert has_text? "A.B.S."
   end
 end
-
