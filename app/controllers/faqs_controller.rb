5a201a510889a711f52075982b6c5c0465a4f0bf
Ruslan Kornev <oganer@gmail.com>
2015-09-24 21:01:04 +0500

    Faqs с view тестами

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,8 +1,14 @@
 class FaqsController < ApplicationController
+  skip_before_action :find_resource, :find_resources
 
   def index
     @topics = get_faqs.
       reject{|topic| topic['closed']}
+  end
+
+  def show
+    @topic = get_faqs.select{|faq| faq['id'] == params[:id].to_i}.first
+    render 'show', layout: 'lightweight'
   end
 
 end
---
+++ test/controllers/faqs_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class FaqsControllerTest < ActionDispatch::IntegrationTest
+
+  test 'Мы можем просмотреть список частых вопросов' do
+    get faqs_path
+    assert_select '.panel-group' do
+      assert_select '#faq-panel-14.panel.panel-default' do
+        assert_select '.panel-heading[data-clickable-object]' do
+          assert_select '.panel-title' do
+            assert_select 'a[data-toggle="collapse"][data-clickable-href][data-parent="#faqs"][href="#faq-collapse-14"]', text: 'Описание раздела Частые вопросы'
+          end
+        end
+        assert_select '#faq-collapse-14.panel-collapse.collapse' do
+          assert_select '.panel-body.discourse-cooked' do
+            assert_select '*', text: /В данном разделе мы размещаем наиболее/
+            assert_select "a:match('href', ?)", /\/faqs\/14/ do |a|
+              assert_select '*', text: 'Открыть в новом окне'
+            end
+          end
+        end
+      end
+    end
+  end
+
+  test 'Просмотр частого вопроса' do
+    get faq_path(14)
+    assert_select '[screen-margin]' do
+      assert_select 'h1', 'Описание раздела Частые вопросы'
+      assert_select '.hidden-print' do
+        assert_select 'a[href="#"][onclick="window.print()"]', text: 'Распечатать'
+      end
+      assert_select '.discourse-cooked' do
+        assert_select '*', text: /В данном разделе мы размещаем наиболее/
+      end
+    end
+    assert_template 'lightweight'
+  end
+end
