3aa52dad137ae8599525200503ce079883b79b9a
Ruslan Kornev <oganer@gmail.com>
2015-09-26 02:09:44 +0500

    Мелочи в админке brands, users

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -7,4 +7,10 @@ class Admin::BrandsController < ApplicationController
     @resource_class = Brand
   end
 
+  def find_resources
+    super
+    @q = @resources.ransack(params[:q])
+    @resources = @q.result(distinct: true)
+  end
+
 end
